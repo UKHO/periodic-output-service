5080671a172ba915f580f4be5d0c30e3e9991b1a
Vishal Dukare <Vishal14583@Mastek.Com>
2022-06-06 16:01:42 +0530

    48898-initial skeleton AB#48898 AB#48922

---
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Fulfilment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UKHO.PeriodicOutputService.Fulfilment
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
