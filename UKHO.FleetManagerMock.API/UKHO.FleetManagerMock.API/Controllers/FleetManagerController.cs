3cebb744fddf97129ed7564b8cb121c4b0d70f2c
Harshda Joshi <harshda15125@mastek.com>
2022-06-23 20:31:06 +0530

    49720-Updated controller method #AB50424

--- UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
+++ UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 using UKHO.FleetManagerMock.API.Models;
 
@@ -36,7 +38,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
             if (userPass == base64Credentials)
             {
 
-                var token = "{token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNjZkMGM1Zi01N2IyLTQ4ZTUtOWNlZS02YjE4YjE2YzJjNWYiLCJpYXQiOiIxNjU1OTA1NTYzIiwic3ViIjoiQjJCTWFzdGVrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkIyQk1hc3RlayIsInVzZXJpZCI6IjUxNjAyIiwic2VjcmV0IjoiMTAwMDpVZ05XSjc5MkpTNVNJMXlkYkxrMmVQSmlQQzVJN1pVWDpqRG5QdnMvNGtxclRRalBGd085aG9IL3B4anRFKzJSeiIsImVuZHBvaW50L0dldCBDYXRhbG9ndWVzIjoidHJ1ZSIsImVuZHBvaW50L0dldCBUb2tlbiB3aXRoIExvZ2luIjoidHJ1ZSIsImVuZHBvaW50L1JlbmV3IFRva2VuIjoidHJ1ZSIsImV4cCI6MTY1NTkwOTE2MywiaXNzIjoiVUtIT19GTSIsImF1ZCI6IlVLSE9fRk0ifQ.-pDCe2NLzppbK79BA3S9_6AlUwOdAAeYDUkrRJdTtgs}";
+                var token = "{token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNjZkMGM1Zi01N2IyLTQ4ZTUtOWNlZS02YjE4YjE2YzJjNWYiLCJpYXQiOiIxNjU1OTA1NTYzIiwic3ViIjoiQjJCTWFzdGVrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkIyQk1hc3RlayIsInVzZXJpZCI6IjUxNjAyIiwic2VjcmV0IjoiMTAwMDpVZ05XSjc5MkpTNVNJMXlkYkxrMmVQSmlQQzVJN1pVWDpqRG5QdnMvNGtxclRRalBGd085aG9IL3B4anRFKzJSeiIsImVuZHBvaW50L0dldCBDYXRhbG9ndWVzIjoidHJ1ZSIsImVuZHBvaW50L0dldCBUb2tlbiB3aXRoIExvZ2luIjoidHJ1ZSIsImVuZHBvaW50L1JlbmV3IFRva2VuIjoidHJ1ZSIsImV4cCI6MTY1NTkwOTE2MywiaXNzIjoiVUtIT19GTSIsImF1ZCI6IlVLSE9fRk0ifQ.-pDCe2NLzppbK79BA3S9_6AlUwOdAAeYDUkrRJdTtgs}";
                 //await GetJwtAuthUnpToken(HttpMethod.Get, base64Credentials , subscriptionKey) ;
                 httpResponse.StatusCode = System.Net.HttpStatusCode.OK;
                 // fleetMangerGetAuthTokenResponse.StatusCode = httpResponse.StatusCode;
@@ -54,8 +56,17 @@ namespace UKHO.FleetManagerMock.API.Controllers
         [Route("/catalogues/{catalogueId}")]
         public IActionResult GetCatalogue([FromHeader(Name = "token")] string token, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string token1)
         {
-           //ToDo            
-            return null;
+            string path = this.configuration.GetSection("FileDirectoryPath")["response"];
+            XDocument doc = XDocument.Load(path);
+            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            Encoding encoding = Encoding.UTF8;
+            byte[] docAsBytes = encoding.GetBytes(doc.ToString());
+            if (docAsBytes == null)
+            {
+                httpResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                return BadRequest(httpResponse.StatusCode);
+            }
+            return File(docAsBytes, "application/xml", path);
         }
 
         [NonAction]
