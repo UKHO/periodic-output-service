e52141c98b948e729e7825462e84a4fa76fffb28
Harshda Joshi <harshda15125@mastek.com>
2022-06-24 12:30:29 +0530

    49720-Added Endpoints #AB50426

--- UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
+++ UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
@@ -1,7 +1,7 @@
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
-using UKHO.FleetManagerMock.API.Models;
+
 
 namespace UKHO.FleetManagerMock.API.Controllers
 {
@@ -18,61 +18,70 @@ namespace UKHO.FleetManagerMock.API.Controllers
 
         [HttpGet]
         [Route("/auth/unp")]
-        public IActionResult GetJwtAuthUnpToken([FromHeader(Name = "userPass")] string userPass, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string userPass1)
+        public IActionResult GetJwtAuthUnpToken([FromHeader(Name = "userPass")] string userPass, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string subscriptionKey)
         {
-            Dictionary<string, string> requestHeaders = new Dictionary<string, string>();
-            foreach (var header in Request.Headers)
+            Dictionary<string, string> requestHeaders = new();
+            foreach (KeyValuePair<string, Microsoft.Extensions.Primitives.StringValues> header in Request.Headers)
             {
                 requestHeaders.Add(header.Key, header.Value);
             }
 
-            string userName = this.configuration.GetSection("FleetManagerB2BApiConfiguration")["userName"];
-            string password = this.configuration.GetSection("FleetManagerB2BApiConfiguration")["password"];
-            string subscriptionkey = this.configuration.GetSection("FleetManagerB2BApiConfiguration")["subscriptionKey"];
-
-            string responseContent = string.Empty;
-            FleetManagerGetAuthTokenResponse fleetMangerGetAuthTokenResponse = new FleetManagerGetAuthTokenResponse();
+            string userName = configuration.GetSection("FleetManagerB2BApiConfiguration")["userName"];
+            string password = configuration.GetSection("FleetManagerB2BApiConfiguration")["password"];
+           
             string base64Credentials = GetBase64EncodedCredentials(userName, password);
-            HttpResponseMessage httpResponse = new HttpResponseMessage();
+            HttpResponseMessage httpResponse = new();
             string AuthToken = string.Empty;
             if (userPass == base64Credentials)
             {
 
-                var token = "{token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNjZkMGM1Zi01N2IyLTQ4ZTUtOWNlZS02YjE4YjE2YzJjNWYiLCJpYXQiOiIxNjU1OTA1NTYzIiwic3ViIjoiQjJCTWFzdGVrIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkIyQk1hc3RlayIsInVzZXJpZCI6IjUxNjAyIiwic2VjcmV0IjoiMTAwMDpVZ05XSjc5MkpTNVNJMXlkYkxrMmVQSmlQQzVJN1pVWDpqRG5QdnMvNGtxclRRalBGd085aG9IL3B4anRFKzJSeiIsImVuZHBvaW50L0dldCBDYXRhbG9ndWVzIjoidHJ1ZSIsImVuZHBvaW50L0dldCBUb2tlbiB3aXRoIExvZ2luIjoidHJ1ZSIsImVuZHBvaW50L1JlbmV3IFRva2VuIjoidHJ1ZSIsImV4cCI6MTY1NTkwOTE2MywiaXNzIjoiVUtIT19GTSIsImF1ZCI6IlVLSE9fRk0ifQ.-pDCe2NLzppbK79BA3S9_6AlUwOdAAeYDUkrRJdTtgs}";
-                //await GetJwtAuthUnpToken(HttpMethod.Get, base64Credentials , subscriptionKey) ;
-                httpResponse.StatusCode = System.Net.HttpStatusCode.OK;
-                // fleetMangerGetAuthTokenResponse.StatusCode = httpResponse.StatusCode;
+               string? token = "token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+               httpResponse.StatusCode = System.Net.HttpStatusCode.OK;
 
-                if (httpResponse.IsSuccessStatusCode)
+               if (httpResponse.IsSuccessStatusCode)
                 {
-                    responseContent = token.Trim();
-                    AuthToken = responseContent;
+                   AuthToken = token;
                 }
+                
+            }
+            else
+            {
+                return BadRequest(httpResponse.StatusCode);
             }
             return Ok(AuthToken);
         }
 
         [HttpGet]
         [Route("/catalogues/{catalogueId}")]
-        public IActionResult GetCatalogue([FromHeader(Name = "token")] string token, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string token1)
+        public IActionResult GetCatalogue([FromHeader(Name = "token")] string token, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string subscriptionkey)
         {
-            string path = this.configuration.GetSection("FileDirectoryPath")["response"];
-            XDocument doc = XDocument.Load(path);
-            HttpResponseMessage httpResponse = new HttpResponseMessage();
-            Encoding encoding = Encoding.UTF8;
-            byte[] docAsBytes = encoding.GetBytes(doc.ToString());
-            if (docAsBytes == null)
+            Dictionary<string, string> requestHeaders = new();
+            foreach (KeyValuePair<string, Microsoft.Extensions.Primitives.StringValues> header in Request.Headers)
             {
-                httpResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
-                return BadRequest(httpResponse.StatusCode);
+                requestHeaders.Add(header.Key, header.Value);
             }
-            return File(docAsBytes, "application/xml", path);
+            string path = this.configuration.GetSection("FileDirectoryPath")["response"];
+            if (token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9}")
+            {
+                XDocument doc = XDocument.Load(path);
+                HttpResponseMessage httpResponse = new();
+                Encoding encoding = Encoding.UTF8;
+                byte[] docAsBytes = encoding.GetBytes(doc.ToString());
+
+                if (docAsBytes != null)
+                {
+                    httpResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                    return File(docAsBytes, "application/xml", path);
+                    
+                }
+            }
+            return Unauthorized();
         }
 
         [NonAction]
         public static string GetBase64EncodedCredentials(string userName, string password)
         {
-            var userCredentialsBytes = System.Text.Encoding.UTF8.GetBytes(userName + ":" + password);
+            byte[]? userCredentialsBytes = System.Text.Encoding.UTF8.GetBytes(userName + ":" + password);
             return Convert.ToBase64String(userCredentialsBytes);
         }
 
