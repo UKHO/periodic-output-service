31ce4dcd730f4cd6da4db9f4234d5507382358e1
Kishor Ambare <Kishor13892@Mastek.Com>
2022-06-27 15:43:51 +0530

    Functional Automated Tests for API

---
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.API.FunctionalTests/Helpers/CommonHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace UKHO.PeriodicOutputService.API.FunctionalTests.Helpers
+{
+    public static class CommonHelper
+    {
+        public static async Task<string> DeserializeAsyncToken(this HttpResponseMessage httpResponseMessage)
+        {
+            string bodyJson = await httpResponseMessage.Content.ReadAsStringAsync();
+            dynamic response = JsonConvert.DeserializeObject<dynamic>(bodyJson);
+            return response.token;
+
+        }
+
+        public static async Task<string> DeserializeAsyncMessage(this HttpResponseMessage httpResponseMessage)
+        {
+            string bodyJson = await httpResponseMessage.Content.ReadAsStringAsync();
+            dynamic response = JsonConvert.DeserializeObject<dynamic>(bodyJson);
+            return response.message;
+
+        }
+
+        public static async Task<string> ReadAsStringAsync(this HttpResponseMessage httpResponseMessage)
+        {
+            string bodyJson = await httpResponseMessage.Content.ReadAsStringAsync();
+            return bodyJson;
+        }
+
+        public static async Task<dynamic> XmlReadAsynch(string xmlreponse)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlreponse);
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            dynamic test = JsonConvert.DeserializeObject(jsonText);
+            return test;
+        }
+
+        public static string getbase64encodedcredentials(string username, string password)
+        {
+            var userCredentialsBytes = System.Text.Encoding.UTF8.GetBytes(username + ":" + password);
+            return Convert.ToBase64String(userCredentialsBytes);
+        }
+    }
+}
---
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.API.FunctionalTests/Helpers/TestConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace UKHO.PeriodicOutputService.API.FunctionalTests.Helpers
+{
+    public class TestConfiguration
+    {
+        protected IConfigurationRoot ConfigurationRoot;
+        public FleetManagerB2BApiConfiguration fleetManagerB2BConfig = new FleetManagerB2BApiConfiguration();
+        public class FleetManagerB2BApiConfiguration
+        {
+            public string userName { get; set; }
+            public string password { get; set; }
+
+            public string baseUrl { get; set; }
+
+            public string subscriptionKey { get; set; }
+
+            public string jwtAuthUnpEndpoint { get; set; }
+
+            public string jwtAuthJwtEndpoint { get; set; }
+
+            public string catalogueEndpoint { get; set; }
+
+            public string invalidjwttoken { get; set; }
+
+            public string nulljwttoken { get; set; }
+
+            public string invalidsubscriptionkey { get; set; }
+
+            public string nullsubscriptionkey { get; set; }
+
+        }
+
+        public TestConfiguration()
+        {
+            ConfigurationRoot = new ConfigurationBuilder()
+                               .AddJsonFile("appsettings.json", false)
+                               .Build();
+
+            ConfigurationRoot.Bind("FleetManagerB2BApiConfiguration", fleetManagerB2BConfig);
+        }
+
+    }
+}
