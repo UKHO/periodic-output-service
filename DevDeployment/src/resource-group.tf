684a3b0b2728eff34379cdf4c32b43f553dbb743
Surendra Behara <surendra.behara@mastek.com>
2022-07-01 21:38:27 +0530

    updated the resource group

--- DevDeployment/src/resource-group.tf
+++ DevDeployment/src/resource-group.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "webapp_rg" {
-  name     = "${local.service_name}-${local.env_name}-webapp-rg"
+  name     = "${local.service_name}-${local.env_name}-rg"
   location = var.location
   tags     = local.tags
 }
 
 resource "azurerm_resource_group" "mock_webapp_rg" {
-  name     = "${local.service_name}-${local.env_name}-mock-webapp-rg"
+  name     = "${local.service_name}-${local.env_name}-mock-rg"
   location = var.location
   tags     = local.tags
 }
--- QALiveDeployment/src/resource-group.tf
+++ QALiveDeployment/src/resource-group.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "webapp_rg" {
   name     = "${var.resource_group_name}-${local.env_name}-rg"
   location = var.location
   tags     = local.tags
