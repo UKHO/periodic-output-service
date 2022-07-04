c874203e0990dd76dc536c2df557762aef25519c
Surendra Behara <surendra.behara@mastek.com>
2022-07-04 18:25:18 +0530

    removed isOutput true from ps script AB#50365

--- DevDeployment/terraform_conditional_run.ps1
+++ DevDeployment/terraform_conditional_run.ps1
@@ -54,9 +54,9 @@ $terraformOutput = terraform output -json | ConvertFrom-Json
 write-output "Set JSON output into pipeline variables"
 Write-Host "##vso[task.setvariable variable=Website_Url;isOutput=true]$($terraformOutput.Website_Url.value)"
 Write-Host "##vso[task.setvariable variable=WEB_APP_NAME;isOutput=true]$($terraformOutput.web_app_name.value)"
-Write-Host "##vso[task.setvariable variable=mockWebAppName;isOutput=true]$($terraformOutput.mock_webappname.value)"
+Write-Host "##vso[task.setvariable variable=mockWebAppName]$($terraformOutput.mock_webappname.value)"
 Write-Host "##vso[task.setvariable variable=FleetManagerB2BApiConfiguration.baseUrl;isOutput=true]$($terraformOutput.fm_mock_web_app_url.value)"
-Write-Host "##vso[task.setvariable variable=mockWebAppResourceGroup;isOutput=true]$($terraformOutput.mock_webapp_rg.value)"
+Write-Host "##vso[task.setvariable variable=mockWebAppResourceGroup]$($terraformOutput.mock_webapp_rg.value)"
 Write-Host "##vso[task.setvariable variable=RG;isOutput=true]$($terraformOutput.webapp_rg.value)"
 
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
