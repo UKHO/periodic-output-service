358c15c13c3290cd172beb5bda379e17d7e30ffe
Surendra Behara <surendra.behara@mastek.com>
2022-07-05 16:16:54 +0530

    updated tf file to remove kv AB#50365

--- QALiveDeployment/src/azure.tf
+++ QALiveDeployment/src/azure.tf
@@ -15,9 +15,15 @@ terraform {
 
 provider "azurerm" {
   features {
+
     resource_group {
        prevent_deletion_if_contains_resources = false
     }
+
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+    }
+
   }
 }
 
