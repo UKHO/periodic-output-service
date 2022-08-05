f9e57ca17c199770b9e88d3e396cc33fddc33a72
Sumod Mohan Edathara <80677469+sumod13943@users.noreply.github.com>
2022-08-05 18:45:20 +0530

    54239 fleet manager stub for ft (#26)
    * 54239 Fleet manager stub for FT's AB#54239 AB#54241 AB#54243 AB#54244
    
    * 54239 configuration changes AB#54239 AB#54244
    
    * 54239 code reviews resolved AB#54239
    
    Co-authored-by: Sumod Edathara <Sumod13943@Mastek.Com>

--- UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
+++ UKHO.FleetManagerMock.API/UKHO.FleetManagerMock.API/Controllers/FleetManagerController.cs
@@ -1,12 +1,11 @@
 using System.Globalization;
+using System.Net;
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using UKHO.FleetManagerMock.API.Common;
-using System.Net;
-using Microsoft.Extensions.Primitives;
 
 namespace UKHO.FleetManagerMock.API.Controllers
 {
@@ -28,11 +27,6 @@ namespace UKHO.FleetManagerMock.API.Controllers
         [Route("/auth/unp")]
         public IActionResult GetJwtAuthUnpToken([FromHeader(Name = "userPass")] string userPass, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string? subscriptionKey)
         {
-            Dictionary<string, string> requestHeaders = new();
-            foreach (KeyValuePair<string, StringValues> header in Request.Headers)
-            {
-                requestHeaders.Add(header.Key, header.Value);
-            }
             string? userName = _fleetManagerApiConfiguration.Value.UserName;
             string? password = _fleetManagerApiConfiguration.Value.Password;
             string? fleetManagerStubSubscriptionKey = _fleetManagerApiConfiguration.Value.SubscriptionKey;
@@ -41,7 +35,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
             string AuthToken = string.Empty;
             if (string.IsNullOrEmpty(subscriptionKey))
             {
-                string? invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
                 httpResponse.StatusCode = HttpStatusCode.Unauthorized;
                 if (!httpResponse.IsSuccessStatusCode)
                 {
@@ -52,7 +46,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
             {
                 if (userPass == base64Credentials)
                 {
-                    string? authTokenResponse = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\", \"expiration\":\"" + DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture) + "\"}";
+                    string authTokenResponse = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\", \"expiration\":\"" + DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture) + "\"}";
                     httpResponse.StatusCode = HttpStatusCode.OK;
                     if (httpResponse.IsSuccessStatusCode)
                     {
@@ -61,7 +55,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
                 }
                 else
                 {
-                    string? invalidUsernamePasswordResponse = "{\"correlationId\":\"10ce5b89-67f4-4090-8364-7ba69eb7cb3d\",\"errors\":[{\"source\":\"userPass\",\"description\":\"Missing or invalid encoded User:Pass.\"}]}";
+                    const string invalidUsernamePasswordResponse = "{\"correlationId\":\"10ce5b89-67f4-4090-8364-7ba69eb7cb3d\",\"errors\":[{\"source\":\"userPass\",\"description\":\"Missing or invalid encoded User:Pass.\"}]}";
                     httpResponse.StatusCode = HttpStatusCode.BadRequest;
                     if (!httpResponse.IsSuccessStatusCode)
                     {
@@ -71,7 +65,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
             }
             else
             {
-                string? invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
                 httpResponse.StatusCode = HttpStatusCode.Unauthorized;
                 if (!httpResponse.IsSuccessStatusCode)
                 {
@@ -85,18 +79,13 @@ namespace UKHO.FleetManagerMock.API.Controllers
         [Route("/catalogues/{catalogueId}")]
         public IActionResult GetCatalogue([FromHeader(Name = "token")] string? token, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string? subscriptionKey)
         {
-            Dictionary<string, string> requestHeaders = new();
-            foreach (KeyValuePair<string, StringValues> header in Request.Headers)
-            {
-                requestHeaders.Add(header.Key, header.Value);
-            }
             string? fleetManagerStubSubscriptionKey = _fleetManagerApiConfiguration.Value.SubscriptionKey;
             string? path = _fileDirectoryPathConfiguration.Value.AVCSCatalogDataFilePath;
             HttpResponseMessage httpResponse = new();
 
             if (string.IsNullOrEmpty(subscriptionKey))
             {
-                string? invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
                 httpResponse.StatusCode = HttpStatusCode.Unauthorized;
                 if (!httpResponse.IsSuccessStatusCode)
                 {
@@ -125,7 +114,7 @@ namespace UKHO.FleetManagerMock.API.Controllers
             }
             else
             {
-                string? invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
                 httpResponse.StatusCode = HttpStatusCode.Unauthorized;
                 if (!httpResponse.IsSuccessStatusCode)
                 {
@@ -136,6 +125,109 @@ namespace UKHO.FleetManagerMock.API.Controllers
             return Unauthorized();
         }
 
+
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("/ft/auth/unp")]
+        public IActionResult GetJwtAuthUnpTokenForFT([FromHeader(Name = "userPass")] string userPass, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string? subscriptionKey)
+        {
+            string? userName = _fleetManagerApiConfiguration.Value.UserName;
+            string? password = _fleetManagerApiConfiguration.Value.Password;
+            string? fleetManagerStubSubscriptionKey = _fleetManagerApiConfiguration.Value.SubscriptionKey;
+            string base64Credentials = GetBase64EncodedCredentials(userName, password);
+            HttpResponseMessage httpResponse = new();
+            string AuthToken = string.Empty;
+            if (string.IsNullOrEmpty(subscriptionKey))
+            {
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
+                httpResponse.StatusCode = HttpStatusCode.Unauthorized;
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    return Unauthorized(JsonConvert.DeserializeObject<InvalidSubscriptionKeyResponse>(invalidSubscriptionKeyResponse));
+                }
+            }
+            if (subscriptionKey == fleetManagerStubSubscriptionKey)
+            {
+                if (userPass == base64Credentials)
+                {
+                    string authTokenResponse = "{\"token\": \"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\", \"expiration\":\"" + DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture) + "\"}";
+                    httpResponse.StatusCode = HttpStatusCode.OK;
+                    if (httpResponse.IsSuccessStatusCode)
+                    {
+                        AuthToken = authTokenResponse;
+                    }
+                }
+                else
+                {
+                    const string invalidUsernamePasswordResponse = "{\"correlationId\":\"10ce5b89-67f4-4090-8364-7ba69eb7cb3d\",\"errors\":[{\"source\":\"userPass\",\"description\":\"Missing or invalid encoded User:Pass.\"}]}";
+                    httpResponse.StatusCode = HttpStatusCode.BadRequest;
+                    if (!httpResponse.IsSuccessStatusCode)
+                    {
+                        return BadRequest(JsonConvert.DeserializeObject<InvalidUsernamePasswordResponse>(invalidUsernamePasswordResponse));
+                    }
+                }
+            }
+            else
+            {
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
+                httpResponse.StatusCode = HttpStatusCode.Unauthorized;
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    return Unauthorized(JsonConvert.DeserializeObject<InvalidSubscriptionKeyResponse>(invalidSubscriptionKeyResponse));
+                }
+            }
+            return Ok(JsonConvert.DeserializeObject<JwtAuthUnpToken>(AuthToken));
+        }
+
+        [HttpGet]
+        [Route("/ft/catalogues/{catalogueId}")]
+        public IActionResult GetCatalogueForFT([FromHeader(Name = "token")] string? token, [FromHeader(Name = "Ocp-Apim-Subscription-Key")] string? subscriptionKey)
+        {
+            string? fleetManagerStubSubscriptionKey = _fleetManagerApiConfiguration.Value.SubscriptionKey;
+            string? path = _fileDirectoryPathConfiguration.Value.FunctionalTestAVCSCatalogDataFilePath;
+            HttpResponseMessage httpResponse = new();
+
+            if (string.IsNullOrEmpty(subscriptionKey))
+            {
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.\"}";
+                httpResponse.StatusCode = HttpStatusCode.Unauthorized;
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    return Unauthorized(JsonConvert.DeserializeObject<InvalidSubscriptionKeyResponse>(invalidSubscriptionKeyResponse));
+                }
+            }
+            if (subscriptionKey == fleetManagerStubSubscriptionKey)
+            {
+                if (token == _jwtUnpAuthToken && !string.IsNullOrEmpty(path))
+                {
+                    XDocument doc = XDocument.Load(path);
+                    Encoding encoding = Encoding.UTF8;
+                    byte[] docAsBytes = encoding.GetBytes(doc.ToString());
+
+                    if (docAsBytes != null)
+                    {
+                        httpResponse.StatusCode = HttpStatusCode.OK;
+                        return File(docAsBytes, "application/xml", path);
+                    }
+                }
+                else
+                {
+                    httpResponse.StatusCode = HttpStatusCode.Forbidden;
+                    return StatusCode(403);
+                }
+            }
+            else
+            {
+                const string invalidSubscriptionKeyResponse = "{\"statusCode\": 401, \"message\":\"Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.\"}";
+                httpResponse.StatusCode = HttpStatusCode.Unauthorized;
+                if (!httpResponse.IsSuccessStatusCode)
+                {
+                    return Unauthorized(JsonConvert.DeserializeObject<InvalidSubscriptionKeyResponse>(invalidSubscriptionKeyResponse));
+                }
+            }
+
+            return Unauthorized();
+        }
         [NonAction]
         public static string GetBase64EncodedCredentials(string? userName, string? password)
         {
