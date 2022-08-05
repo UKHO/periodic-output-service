a02deecda3e895ea7bd64c43f757a8c2807fc398
Vishal Dukare <Vishal14583@Mastek.Com>
2022-08-05 20:31:11 +0530

    52226-ess service changes AB#52226

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Fulfilment/Services/EssService.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Fulfilment/Services/EssService.cs
@@ -5,6 +5,7 @@ using UKHO.PeriodicOutputService.Common.Helpers;
 using UKHO.PeriodicOutputService.Fulfilment.Configuration;
 using UKHO.PeriodicOutputService.Common.Logging;
 using UKHO.PeriodicOutputService.Fulfilment.Models;
+using UKHO.ExchangeSetService.Common.Logging;
 
 namespace UKHO.PeriodicOutputService.Fulfilment.Services
 {
@@ -28,31 +29,24 @@ namespace UKHO.PeriodicOutputService.Fulfilment.Services
 
         public async Task<ExchangeSetResponseModel?> PostProductIdentifiersData(List<string> productIdentifiers)
         {
-            _logger.LogInformation(EventIds.GetAccessTokenForESSEndPointStarted.ToEventId(), "Getting access token to call ESS endpoint started at {DateTime} | _X-Correlation-ID:{CorrelationId}", DateTime.Now.ToUniversalTime(), CommonHelper.CorrelationID);
+            _logger.LogInformation(EventIds.PostProductIdentifiersToEssStarted.ToEventId(), "Request to post {ProductIdentifiersCount} productidentifiers to ESS started | {DateTime} | _X-Correlation-ID : {CorrelationId}", productIdentifiers.Count.ToString(), DateTime.Now.ToUniversalTime(), CommonHelper.CorrelationID);
 
+            //string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSIsImtpZCI6IjJaUXBKM1VwYmpBWVhZR2FYRUpsOGxWMFRPSSJ9.eyJhdWQiOiI4MGE2YzY4Yi01OWFhLTQ5YTQtOTM5YS03OTY4ZmY3OWQ2NzYiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC85MTM0Y2E0OC02NjNkLTRhMDUtOTY4YS0zMWE0MmYwYWVkM2UvIiwiaWF0IjoxNjU5NzA5NTY0LCJuYmYiOjE2NTk3MDk1NjQsImV4cCI6MTY1OTcxNDgzOCwiYWNyIjoiMSIsImFpbyI6IkFZUUFlLzhUQUFBQXdKZ2FpYnlRTVNMOWxlTHhoeDZIS1AyaHMrRlhkTjVjZXpqMjU0bzdDSFdtUi9RbnRSbnRuUjQ0cHdaUG4zUlQrbXFieVpONHQzRkJPWUZVeXA0TkNpcll4RTg4NzJabWs0VmRpZmNibWM4L0hlcDV6ai81YnFCUzZaa2h6ZjBQNEowR090M1ZBd2lidTNOTUtITmkwY2RoaTY5MWhoNndZbDQvRVhOSTlWUT0iLCJhbXIiOlsicHdkIiwibWZhIl0sImFwcGlkIjoiODBhNmM2OGItNTlhYS00OWE0LTkzOWEtNzk2OGZmNzlkNjc2IiwiYXBwaWRhY3IiOiIwIiwiZW1haWwiOiJWaXNoYWwxNDU4M0BtYXN0ZWsuY29tIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYWRkMWM1MDAtYTZkNy00ZGJkLWI4OTAtN2Y4Y2I2ZjdkODYxLyIsImlwYWRkciI6IjE2My4xMTYuMjA1LjExOSIsIm5hbWUiOiJWaXNoYWwgRHVrYXJlIiwib2lkIjoiODZjNWVlN2EtOTRlNi00OGM2LTlmOWMtYTRhYzU3MDRkZTFiIiwicHdkX2V4cCI6IjU2OTg0MSIsInB3ZF91cmwiOiJodHRwczovL3BvcnRhbC5taWNyb3NvZnRvbmxpbmUuY29tL0NoYW5nZVBhc3N3b3JkLmFzcHgiLCJyaCI6IjAuQVFJQVNNbzBrVDFtQlVxV2lqR2tMd3J0UG92R3BvQ3FXYVJKazVwNWFQOTUxbllDQUI4LiIsInJvbGVzIjpbIkV4Y2hhbmdlU2V0U2VydmljZVVzZXIiXSwic2NwIjoiVXNlci5SZWFkIiwic3ViIjoiSWMtRFpLYUFONlZfSWJCZVk1cDc4UWtZV052N2xOUUVmc3NWT1NpTjBxVSIsInRpZCI6IjkxMzRjYTQ4LTY2M2QtNGEwNS05NjhhLTMxYTQyZjBhZWQzZSIsInVuaXF1ZV9uYW1lIjoiVmlzaGFsMTQ1ODNAbWFzdGVrLmNvbSIsInV0aSI6IjRfOHpGaGpRcDBTSm5rVEExY3lNQUEiLCJ2ZXIiOiIxLjAifQ.NlDuBAY-nKH_2_JocK6j4DwEvhP3cbkESNAUZ8g0j4CFP5YJzteDaGfFAeAfZdr2xlW1TgUYIoePhJw_w5SaSX9DJ74yX4hcOPN4Vxw7cO66K8rs5jYGK2D-rD3yQjqXbg9flWV8B1ypbdAXi9hqAYOr_v9UynOFfhEMbX8s_MQHidAIePqE5msElYH2uoM_AmQMUAewL5-Ntn1mxR0NUanbYmuAoMdxettDlGVuzxj3FuZvYUeRFO8owzdYA26oKwWFnYnFo_n_kw5v4iY3TnMMcb2kXR3hqHp2_lt9uGW_KsTV_HEiAvgr-eWutUulSieOURHh1bHfOXxK81WX7A";
             string accessToken = await _authEssTokenProvider.GetManagedIdentityAuthAsync(_essApiConfiguration.Value.EssClientId);
-
-            _logger.LogInformation(EventIds.GetAccessTokenForESSEndPointCompleted.ToEventId(), "Getting access token to call ESS endpoint completed at {DateTime} | _X-Correlation-ID:{CorrelationId}", DateTime.Now.ToUniversalTime(), CommonHelper.CorrelationID);
-
-            ExchangeSetResponseModel? exchangeSetGetBatchResponse = new();
-
-            _logger.LogInformation(EventIds.ExchangeSetPostProductIdentifiersRequestStarted.ToEventId(), "Request to post productidentifiers to ESS started at {DateTime} | _X-Correlation-ID:{CorrelationId}", DateTime.Now.ToUniversalTime(), CommonHelper.CorrelationID);
 
             HttpResponseMessage httpResponse = await _essApiClient.PostProductIdentifiersDataAsync(_essApiConfiguration.Value.BaseUrl, productIdentifiers, accessToken);
 
             if (httpResponse.IsSuccessStatusCode)
             {
                 string bodyJson = await httpResponse.Content.ReadAsStringAsync();
-
-                exchangeSetGetBatchResponse = JsonConvert.DeserializeObject<ExchangeSetResponseModel>(bodyJson);
-
-                _logger.LogInformation(EventIds.ExchangeSetPostProductIdentifiersRequestCompleted.ToEventId(), "Request to post productidentifiers to ESS completed at {DateTime} | StatusCode:{StatusCode} | _X-Correlation-ID:{CorrelationId}", DateTime.Now.ToUniversalTime(), httpResponse.StatusCode.ToString(), CommonHelper.CorrelationID);
+                _logger.LogInformation(EventIds.PostProductIdentifiersToEssCompleted.ToEventId(), "Request to post productidentifiers to ESS completed | {DateTime} | StatusCode : {StatusCode} | _X-Correlation-ID : {CorrelationId}", DateTime.Now.ToUniversalTime(), httpResponse.StatusCode.ToString(), CommonHelper.CorrelationID);
+                return JsonConvert.DeserializeObject<ExchangeSetResponseModel>(bodyJson);
             }
             else
             {
-                _logger.LogError(EventIds.ExchangeSetPostProductIdentifiersFailed.ToEventId(), "Failed to post productidentifiers to ESS at {DateTime} | StatusCode:{StatusCode} | _X-Correlation-ID:{CorrelationId}", DateTime.Now.ToUniversalTime(), httpResponse.StatusCode.ToString(), CommonHelper.CorrelationID);
+                _logger.LogError(EventIds.PostProductIdentifiersToEssFailed.ToEventId(), "Failed to post productidentifiers to ESS | {DateTime} | StatusCode : {StatusCode} | _X-Correlation-ID : {CorrelationId}", DateTime.Now.ToUniversalTime(), httpResponse.StatusCode.ToString(), CommonHelper.CorrelationID);
+                throw new FulfilmentException(EventIds.PostProductIdentifiersToEssFailed.ToEventId());
             }
-            return exchangeSetGetBatchResponse;
         }
     }
 }
