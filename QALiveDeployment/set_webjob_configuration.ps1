d6cb8d7708ee3121974dea59db0efcf73e7901ee
Surendra Behara <surendra.behara@mastek.com>
2022-08-18 19:52:26 +0530

    54314-separated set stub and webjob swap configuration for QA AB#54314 AB#54495

--- QALiveDeployment/set_webjob_configuration.ps1
+++ QALiveDeployment/set_webjob_configuration.ps1
@@ -1,17 +1,11 @@
 param (
     [Parameter(Mandatory = $true)] [string] $essapibaseurl,
     [Parameter(Mandatory = $true)] [string] $resourcegroup,
-    [Parameter(Mandatory = $true)] [string] $mockresourcegroup,
     [Parameter(Mandatory = $true)] [string] $webappname,
-    [Parameter(Mandatory = $true)] [string] $mockwebappname,
     [Parameter(Mandatory = $true)] [string] $fssapibaseurl,
-    [Parameter(Mandatory = $true)] [string] $fleetmanagerbaseurl,
-    [Parameter(Mandatory = $true)] [string] $fleetmanagerfilepath
-)
+    [Parameter(Mandatory = $true)] [string] $fleetmanagerbaseurl
 
-Write-Output "Set Stub Configuration in appsetting..."
-az webapp config appsettings set -g $mockresourcegroup -n $mockwebappname --settings FleetManagerB2BApiConfiguration:GetCatalogueResponseFilePath=$fleetmanagerfilepath
-az webapp restart --name $mockwebappname --resource-group $mockresourcegroup
+)
 
 Write-Output "Set Webjob Configuration in appsetting..."
 az webapp config appsettings set -g $resourcegroup -n $webappname --settings ESSApiConfiguration:BaseUrl=$essapibaseurl FSSApiConfiguration:BaseUrl=$fssapibaseurl FleetManagerB2BApiConfiguration:BaseUrl=$fleetmanagerbaseurl
