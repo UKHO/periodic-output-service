96ff1cfd5a8f63084d4dceb4578a5e636bd50ae5
Sumod Edathara <Sumod13943@Mastek.Com>
2022-08-19 15:10:28 +0530

    52222 unit tests removed AB#52222

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Fulfilment.UnitTests/Helpers/FileSystemHelperTests.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Fulfilment.UnitTests/Helpers/FileSystemHelperTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO.Abstractions;
-using System.Text;
 using FakeItEasy;
 using FluentAssertions;
 using UKHO.PeriodicOutputService.Common.Helpers;
@@ -82,15 +81,6 @@ namespace UKHO.PeriodicOutputService.Fulfilment.UnitTests.Helpers
                             .MustHaveHappenedOnceExactly();
         }
 
-
-        [Test]
-        public void Does_CreateFileCopy_Executes_Successful()
-        {
-            using (var test_Stream = new MemoryStream(Encoding.UTF8.GetBytes("whatever")))
-            {
-                _fileSystemHelper.CreateFileCopy(Path.Combine(filePath, "test.zip"), test_Stream);
-            }
-        }
 
         [Test]
         public void Does_CreateIsoAndSha1_Executes_Successfully()
