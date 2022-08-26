7cc4ea52f359d94e0d9fa0da54dc853f267a49cc
Surendra Behara <97676024+surendrabehara2022@users.noreply.github.com>
2022-08-26 22:00:29 +0530

    54783 devops health endpoint checks (#33)
    * Added initial powershell script to dev files
    
    * Added pipeline steps to run health checks
    
    * 54783-Health check endpoing implemented for fm stub AB#54783 AB#55069
    
    * Fixed health check jobs configurations
    
    * Added dependent variables to the check infra job
    
    * 54783-Subscriptin key removed as not needed for FM Stub AB#54783 AB#55069
    
    * 54783-updated variables and ps script AB#54783 AB#54791
    
    * 54783-updated subscription details AB#54783 AB#54791
    
    * 54783-commented CheckFSS job AB#54783
    
    * 54783-commented depends on AB#54783
    
    * 54783-updated script path AB#54783 AB#54791
    
    * 54783-uncommented CheckFSS job AB#54783 AB#54791
    
    * 54783-updated script path AB#54783 AB#54791
    
    * 54783-updated health check tasks for dev AB#54783 AB#54791
    
    * 54783-added health check tasks for QA stage AB#54783 AB#54791
    
    * 54783- removed task from yml file AB#54783 AB#54791
    
    * 54783-updated subscription details AB#54783 AB#54791
    
    * 54783- added healthcheck in live stage AB#54783 AB#54791
    
    * 54783-uncommented condition in QA stage AB#54783 AB#54791
    
    Co-authored-by: MattWin90 <matthew.winstanley@mastek.com>
    Co-authored-by: harshal11869 <Harshal11869@Mastek.com>
    Co-authored-by: Jason <105857749+ajasonca@users.noreply.github.com>

---
+++ UKHO.FmEssFssMock.API/UKHO.FmEssFssMock.API/HealthChecks/FleetManagerStubHealthCheck.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using UKHO.FmEssFssMock.API.Common;
+
+namespace UKHO.FmEssFssMock.API.HealthChecks
+{
+    [ExcludeFromCodeCoverage]
+    public class FleetManagerStubHealthCheck : IHealthCheck
+    {
+        private readonly IOptions<FleetManagerB2BApiConfiguration> _fleetManagerB2BApiConfiguration;
+
+        public FleetManagerStubHealthCheck(IOptions<FleetManagerB2BApiConfiguration> fleetManagerB2BApiConfiguration)
+        {
+            _fleetManagerB2BApiConfiguration = fleetManagerB2BApiConfiguration;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            if (CheckFileContent().Status == HealthStatus.Healthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("Healthy"));
+            }
+
+            return Task.FromResult(CheckFileContent());
+        }
+
+        private HealthCheckResult CheckFileContent()
+        {
+            string? avcsCatalogueResponseFilePath = _fleetManagerB2BApiConfiguration.Value.GetCatalogueResponseFilePath;
+
+            if (!string.IsNullOrEmpty(avcsCatalogueResponseFilePath))
+            {
+                Encoding encoding = Encoding.UTF8;
+
+                XDocument avcsCatalogDataFile = XDocument.Load(avcsCatalogueResponseFilePath);
+                byte[] avcsCatalogDataFileBytes = encoding.GetBytes(avcsCatalogDataFile.ToString());
+
+                if (avcsCatalogDataFileBytes != null)
+                {
+                    return HealthCheckResult.Healthy();
+                }
+            }
+
+            return HealthCheckResult.Unhealthy();
+        }
+    }
+}
