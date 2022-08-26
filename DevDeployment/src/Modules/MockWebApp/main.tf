00074b8eb527f02bffaf4107db4de3f0ac4871da
Surendra Behara <surendra.behara@mastek.com>
2022-08-26 12:03:58 +0530

    54784-updated main.tf AB#54784 AB#54791

--- DevDeployment/src/Modules/MockWebApp/main.tf
+++ DevDeployment/src/Modules/MockWebApp/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_windows_web_app" "mock_webapp_service" {
   https_only = true
 }
 
-resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
+resource "azurerm_app_service_virtual_network_swift_connection" "mock_webapp_vnet_integration" {
   app_service_id = azurerm_windows_web_app.mock_webapp_service.id
   subnet_id      = var.subnet_id
 }
