c1a5df375c5c30bff6de457ae2b01b63a66f8276
Surendra Behara <surendra.behara@mastek.com>
2022-09-08 18:15:47 +0530

    49774-updated main.tf AB#49774

--- DevDeployment/src/Modules/Storage/main.tf
+++ DevDeployment/src/Modules/Storage/main.tf
@@ -1,17 +1,18 @@
 resource "azurerm_storage_account" "pos_storage" {
-  name                         = lower("${var.service_name}${var.env_name}storageukho")
-  resource_group_name          = var.resource_group_name
-  location                     = var.location
-  account_tier                 = "Standard"
-  account_replication_type     = "LRS"
-  account_kind                 = "StorageV2"
+  name                              = lower("${var.service_name}${var.env_name}storageukho")
+  resource_group_name               = var.resource_group_name
+  location                          = var.location
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
+  account_kind                      = "StorageV2"
+  allow_nested_items_to_be_public   = false
 
-  tags                         = var.tags
+  tags                              = var.tags
 #    network_rules {
-#    default_action             = "Deny"
-#    ip_rules                   = var.allowed_ips
-#    bypass                     = ["Logging", "Metrics", "AzureServices"]
-#    virtual_network_subnet_ids = [var.m_spoke_subnet,var.agent_subnet]
+#    default_action                 = "Deny"
+#    ip_rules                       = var.allowed_ips
+#    bypass                         = ["Logging", "Metrics", "AzureServices"]
+#    virtual_network_subnet_ids     = [var.m_spoke_subnet,var.agent_subnet]
 #}
 }
 
