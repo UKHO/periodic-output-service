ea8fc37002ca90f9e9270266e2abb8241d1495b0
Vishal Dukare <Vishal14583@Mastek.Com>
2022-09-08 11:38:09 +0530

    55348-vnet integration removed for testing purpose AB#55348

--- DevDeployment/src/variables.tf
+++ DevDeployment/src/variables.tf
@@ -36,9 +36,9 @@ variable "spoke_subnet_name" {
   type = string
 }
 
-variable "mock_spoke_subnet_name" {
-  type = string
-}
+#variable "mock_spoke_subnet_name" {
+#  type = string
+#}
 
 variable "agent_rg" {
   type = string
