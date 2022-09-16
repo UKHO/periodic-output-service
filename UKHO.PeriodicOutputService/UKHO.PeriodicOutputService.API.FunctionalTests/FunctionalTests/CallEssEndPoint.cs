1e8ce70ba499a58670de2e9bc24106a0ebc320e5
Vishal Dukare <Vishal14583@Mastek.Com>
2022-09-16 22:47:27 +0530

    55432-minor changes AB#55432

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.API.FunctionalTests/FunctionalTests/CallEssEndPoint.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.API.FunctionalTests/FunctionalTests/CallEssEndPoint.cs
@@ -42,12 +42,13 @@ namespace UKHO.PeriodicOutputService.API.FunctionalTests.FunctionalTests
             unpResponse = await getunp.GetJwtAuthUnpToken(fleet.baseUrl, userCredentialsBytes, fleet.subscriptionKey);
             string unpToken = await unpResponse.DeserializeAsyncToken();
             HttpResponseMessage httpResponse = await getcat.GetCatalogueEndpoint(fleet.baseUrl, unpToken, fleet.subscriptionKey);
-            productIdentifiers = await getcat.GetProductList(httpResponse);
-
-            unpResponse.StatusCode.Should().Be((HttpStatusCode)200, "Catalogue endpoint");
 
             HttpResponseMessage apiResponse = MockHelper.ConfigureFM(posWebJob.MockApiBaseUrl, posWebJob.FMConfigurationValidProductIdentifier);
             apiResponse.StatusCode.Should().Be((HttpStatusCode)200);
+
+            productIdentifiers = await getcat.GetProductList(httpResponse);
+
+            unpResponse.StatusCode.Should().Be((HttpStatusCode)200, "Catalogue endpoint");
 
             await CommonHelper.RunWebJob();
         }
