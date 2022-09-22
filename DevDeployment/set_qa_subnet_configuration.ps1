03f18d093bec73a946698773de821d3ee51444e7
Surendra Behara <surendra.behara@mastek.com>
2022-09-22 17:23:55 +0530

    54784-fixed formatting AB#54784

--- DevDeployment/set_qa_subnet_configuration.ps1
+++ DevDeployment/set_qa_subnet_configuration.ps1
@@ -5,7 +5,7 @@ param (
 
 Write-Output "Set QA Subnet in Mock Network Configuration ..."
 az webapp config access-restriction add -g $mockresourcegroup -n $mockwebappname --action Allow --subnet "/subscriptions/3fbb5f2f-bce2-4d01-a92e-91f4483d939e/resourceGroups/m-spokeconfig-rg/providers/Microsoft.Network/virtualNetworks/ESSQA-vnet/subnets/ess-fulfilment-service-l-1" --priority 65000 --scm-site false -i
-#az webapp restart --name $mockwebappname --resource-group $mockresourcegroup
+
 
 
 
