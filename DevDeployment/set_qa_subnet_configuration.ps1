051af6c3750b8ca1a7bdae97f61b7119b488f666
Surendra Behara <surendra.behara@mastek.com>
2022-09-22 19:53:02 +0530

    57019-removed hardcoded values from script, used through library AB#57019

--- DevDeployment/set_qa_subnet_configuration.ps1
+++ DevDeployment/set_qa_subnet_configuration.ps1
@@ -1,10 +1,14 @@
 param (
     [Parameter(Mandatory = $true)] [string] $mockresourcegroup,
     [Parameter(Mandatory = $true)] [string] $mockwebappname
+    [Parameter(Mandatory = $true)] [string] $qasubscriptionid,
+    [Parameter(Mandatory = $true)] [string] $qavnetresourcegroup,
+    [Parameter(Mandatory = $true)] [string] $qavnetname,
+    [Parameter(Mandatory = $true)] [string] $qasubnetname
 )
 
 Write-Output "Set QA Subnet in Mock Network Configuration ..."
-az webapp config access-restriction add -g $mockresourcegroup -n $mockwebappname --action Allow --subnet "/subscriptions/3fbb5f2f-bce2-4d01-a92e-91f4483d939e/resourceGroups/m-spokeconfig-rg/providers/Microsoft.Network/virtualNetworks/ESSQA-vnet/subnets/ess-fulfilment-service-l-1" --priority 65000 --scm-site false -i
+az webapp config access-restriction add -g $mockresourcegroup -n $mockwebappname --action Allow --subnet "/subscriptions/'$qasubscriptionid'/resourceGroups/'$qavnetresourcegroup'/providers/Microsoft.Network/virtualNetworks/'$qavnetname'/subnets/'$qasubnetname'" --priority 65000 --scm-site false -i
 
 
 
