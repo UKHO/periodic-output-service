9b7825678d3017943b6336f907ca4db3e929821f
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2023-02-17 08:38:06 +0000

    ABZU

--- DevDeployment/add_lease.ps1
+++ DevDeployment/add_lease.ps1
@@ -1,17 +1,18 @@
-$noOfDays = 365 * 2
-
-$contentType = "application/json";
-
-$headers = @{ Authorization = 'Bearer $(System.AccessToken)' };
-
-$rawRequest = @{ daysValid = $noOfDays; definitionId = $(System.DefinitionId); ownerId = 'User:$(Build.RequestedForId)'; protectPipeline = $false; runId = $(Build.BuildId) };
-
-$request = ConvertTo-Json @($rawRequest);
-
-$uri = "$(System.CollectionUri)$(System.TeamProject)/__apis/build/retention/leases?api-version=7.0";
-
 try{
-  Invoke-RestMethod -uri $uri -method POST -Headers $headers -ContentType $contentType -Body $request;
+
+    $noOfDays = 365 * 2
+
+    $contentType = "application/json";
+
+    $headers = @{ Authorization = 'Bearer $(System.AccessToken)' };
+
+    $rawRequest = @{ daysValid = $noOfDays; definitionId = $(System.DefinitionId); ownerId = 'User:$(Build.RequestedForId)'; protectPipeline = $false; runId = $(Build.BuildId) };
+
+    $request = ConvertTo-Json @($rawRequest);
+
+    $uri = "$(System.CollectionUri)$(System.TeamProject)/__apis/build/retention/leases?api-version=7.0";
+
+    Invoke-RestMethod -uri $uri -method POST -Headers $headers -ContentType $contentType -Body $request;
 }
 catch{
    Write-Host $_
