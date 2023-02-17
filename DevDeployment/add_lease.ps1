8e997b129d21eda6fe4e2dc9d1497c2c4b70a982
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2023-02-17 10:12:34 +0000

    ABZU

--- DevDeployment/add_lease.ps1
+++ DevDeployment/add_lease.ps1
@@ -1,17 +1,29 @@
+Param(
+    [Parameter(mandatory=$true)][number]$daysValid,
+	[Parameter(mandatory=$true)][string]$accessToken,
+    [Parameter(mandatory=$true)][string]$definitionId,
+    [Parameter(mandatory=$true)][string]$requestedForId,
+    [Parameter(mandatory=$true)][string]$buildId,
+    [Parameter(mandatory=$true)][string]$collectionUri,
+    [Parameter(mandatory=$true)][string]$teamProject    
+)
+
 try{
 
-    $noOfDays = 365 * 2
+    #$daysValid = 365 * 2
 
     $contentType = "application/json";
 
-    $headers = @{ Authorization = 'Bearer $(System.AccessToken)' };
+    #$headers = @{ Authorization = 'Bearer $(System.AccessToken)' };
+    $headers = @{ Authorization = 'Bearer $accessToken' };
 
-    $rawRequest = @{ daysValid = $noOfDays; definitionId = $(System.DefinitionId); ownerId = 'User:$(Build.RequestedForId)'; protectPipeline = $false; runId = $(Build.BuildId) };
+    #$rawRequest = @{ daysValid = $noOfDays; definitionId = $(System.DefinitionId); ownerId = 'User:$(Build.RequestedForId)'; protectPipeline = $false; runId = $(Build.BuildId) };
+    $rawRequest = @{ daysValid = $daysValid; definitionId = $definitionId; ownerId = 'User:$requestedForId'; protectPipeline = $false; runId = $buildId };
 
     $request = ConvertTo-Json @($rawRequest);
 
-    $uri = "$(System.CollectionUri)$(System.TeamProject)/__apis/build/retention/leases?api-version=7.0";
-
+    $uri = "$collectionUri$teamProject/_apis/build/retention/leases?api-version=7.0";
+    Write-Host $uri
     Invoke-RestMethod -uri $uri -method POST -Headers $headers -ContentType $contentType -Body $request;
 }
 catch{
