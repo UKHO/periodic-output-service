27d1488ffabaa21f7c36fcc82baf5a630b8d4c0e
Daniel Owen <adanielo@Engineering.ukho.gov.uk>
2023-11-13 13:43:48 +0000

    Fix

--- vNextE2EDeployment/terraform_conditional_run.ps1
+++ vNextE2EDeployment/terraform_conditional_run.ps1
@@ -54,9 +54,10 @@ Write-output "Terraform output as json"
 $terraformOutput = terraform output -json | ConvertFrom-Json
 
 write-output "Set JSON output into pipeline variables"
+write-outpu "Testing something"
 Write-Host "##vso[task.setvariable variable=Website_Url]$($terraformOutput.Website_Url.value)"
 Write-Host "##vso[task.setvariable variable=WEB_APP_NAME]$($terraformOutput.web_app_name.value)"
-Write-Host "##vso[task.setvariable variable=mockWebAppResourceGroup;isOutput=true]$($terraformOutput.mock_webapp_rg.value)"
+Write-Host "##vso[task.setvariable variable=mockWebAppResourceGroup]$($terraformOutput.mock_webapp_rg.value)"
 Write-Host "##vso[task.setvariable variable=mockWebApp;isOutput=true]$($terraformOutput.mock_webappname.value)"
 Write-Host "##vso[task.setvariable variable=RGName]$($terraformOutput.webapp_rg.value)"
 Write-Host "##vso[task.setvariable variable=WEBAPP;isOutput=true]$($terraformOutput.web_app_name.value)"
