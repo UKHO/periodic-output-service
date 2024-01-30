2a76019ff2c59b124d525786eb160f76273c14ec
harshal11869 <85306355+harshal11869@users.noreply.github.com>
2024-01-30 21:37:27 +0530

    139799 - Bespoke Exchange Set Configuration Service Project Skeleton Creation (#108)
    * 139799 - bespoke exchange set configuration service skeleton project creation AB#139799 AB#139803
    
    * 139799 - added appsetting file AB#139799 AB#139803
    
    * 139799 - appsetting updated AB#139799 AB#139803
    
    ---------
    
    Co-authored-by: Rohit Chettri <rohit15478@mastek.com>

---
+++ UKHO.PeriodicOutputService/UKHO.BESS.ConfigurationService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace UKHO.BESS.ConfigurationService
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Started");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}{Environment.NewLine} Stack trace: {ex.StackTrace}");
+                throw;
+            }
+        }
+    }
+}
