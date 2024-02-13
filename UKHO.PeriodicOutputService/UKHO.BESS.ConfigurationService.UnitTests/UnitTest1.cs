305ce592964705199162dbfd6f94a0e91811ea9a
ratnas104142 <127212727+ratnas104142@users.noreply.github.com>
2024-02-13 17:15:09 +0530

    140029 - BESS Configuration Service WebJob Logging and Config (#111)
    * 140029 - Added code changes for Configure Azure Key Vault and AppInsights Logger AB#140029 AB#141013 AB#141014
    
    ---------
    
    Co-authored-by: ratnas104142 <ratnas104142@mastek.com>
    Co-authored-by: Abhijeet Naik <abhijee14992@mastek.com>
    Co-authored-by: MadhavTengetol <97515169+MadhavTengetol@users.noreply.github.com>
    Co-authored-by: Pralhad Sakhalkar <pralh1502152@mastek.com>

---
+++ UKHO.PeriodicOutputService/UKHO.BESS.ConfigurationService.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace UKHO.BESS.ConfigurationService.UnitTests
+{
+    //This is a temporary file it will be deleted
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
