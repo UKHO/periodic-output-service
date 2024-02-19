bc21bef4dc22e1548f77381f98d6751276ac43a1
Abhijeet Naik <abhijee14992@mastek.com>
2024-02-19 20:11:48 +0530

    140033 - Added configuration job for BESS AB#140033 AB#143040

---
+++ UKHO.PeriodicOutputService/UKHO.BESS.ConfigurationService/BessConfigurationServiceJob.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using UKHO.BESS.ConfigurationService.Services;
+using UKHO.PeriodicOutputService.Common.Helpers;
+using UKHO.PeriodicOutputService.Common.Logging;
+
+namespace UKHO.BESS.ConfigurationService
+{
+    public class BessConfigurationServiceJob
+    {
+        private readonly ILogger<BessConfigurationServiceJob> logger;
+        private readonly IConfigurationService configurationService;
+
+        public BessConfigurationServiceJob(ILogger<BessConfigurationServiceJob> logger, IConfigurationService configurationService)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configurationService = configurationService;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                logger.LogInformation(EventIds.BessConfigurationServiceStarted.ToEventId(), "BESS Configuration Service Started | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
+
+                configurationService.ProcessConfigs();
+
+                logger.LogInformation(EventIds.BessConfigurationServiceCompleted.ToEventId(), "BESS Configuration Service Completed | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(EventIds.UnhandledException.ToEventId(), "Exception occured while processing Configuration Service webjob with Exception Message : {Message} | StackTrace : {StackTrace} | _X-Correlation-ID : {CorrelationId}", ex.Message, ex.StackTrace, CommonHelper.CorrelationID);
+                throw;
+            }
+        }
+    }
+}
