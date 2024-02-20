f2aa9bfd0f01a7e6fa8624ffcd40636dd031c6de
Abhijeet Naik <abhijee14992@mastek.com>
2024-02-20 11:54:51 +0530

    140033 - Bess model changes as per validation AB#140033

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Models/Bess/BessConfig.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Models/Bess/BessConfig.cs
@@ -13,9 +13,15 @@ namespace UKHO.PeriodicOutputService.Common.Models.Bess
         public string KeyFileType { get; set; }
         public IEnumerable<string> AllowedUsers { get; set; }
         public IEnumerable<string> AllowedUserGroups { get; set; }
-        public IEnumerable<KeyValuePair<string, string>> Tags { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
         public string ReadMeSearchFilter { get; set; }
         public int BatchExpiryInDays { get; set; }
         public bool IsEnabled { get; set; }
+    }
+
+    public class Tag
+    {
+        public string key { get; set; }
+        public string value { get; set; }
     }
 }
