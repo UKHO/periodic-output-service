685fab5e5730723492a50125405a55af6bc6da1f
Chaitali Shidid <chaital14503@mastek.com>
2024-02-20 18:05:07 +0530

    142686 - Cron validation, fluent validation - code refactor #142686 #143258

--- UKHO.PeriodicOutputService/UKHO.BESS.ConfigurationService/Validation/ConfigValidator.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.ConfigurationService/Validation/ConfigValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
+using NCrontab.Advanced;
+using NCrontab.Advanced.Enumerations;
 using UKHO.PeriodicOutputService.Common.Enums;
 using UKHO.PeriodicOutputService.Common.Models.Bess;
 
@@ -13,136 +15,111 @@ namespace UKHO.BESS.ConfigurationService
     {
         public ConfigValidator()
         {
-            RuleFor(config => config.Name).NotNull().WithMessage("attribute is missing").
+            RuleFor(config => config.Name).NotNull().WithMessage("Attribute is missing").
                 DependentRules(() =>
                 {
-                    RuleFor(config => config.Name).NotEmpty().WithMessage("value is not provided")
+                    RuleFor(config => config.Name).NotEmpty().WithMessage("Value is not provided")
                      .DependentRules(() =>
                      {
                          RuleFor(config => config.Name).Length(1, 50).WithMessage("Name should be of max 50 characters length")
-                    .Must(name => IsValidName(name)).WithMessage("should not have characters \\/:*?\"<>|");
+                    .Must(name => IsValidName(name)).WithMessage("Should not have characters \\/:*?\"<>|");
                      });
                 });
 
-            //RuleFor(config => config.IsEncrypted).NotNull().WithMessage("attribute is missing or value is not provided")
-            //         .DependentRules(() =>
-            //         {
-            //             RuleFor(config => config.IsEncrypted).Must(x => x == false || x == true).WithMessage(" Expected value is either true or false");
-            //         });
-
-            RuleFor(config => config.ExchangeSetStandard).NotNull().WithMessage("attribute is missing or value is not provided")
+            RuleFor(config => config.ExchangeSetStandard).NotNull().WithMessage("Attribute is missing or value is not provided")
                      .DependentRules(() =>
                      {
-                         RuleFor(config => config.ExchangeSetStandard).Must(exchangeSetStandard => IsValidExchangeSetStandard(exchangeSetStandard.ToUpper())).WithMessage("attribute value is invalid. Expected value is either s63 or s57");
+                         RuleFor(config => config.ExchangeSetStandard).Must(exchangeSetStandard => IsValidExchangeSetStandard(exchangeSetStandard.ToUpper())).WithMessage("Attribute value is invalid. Expected value is either s63 or s57");
                      });
 
-            RuleFor(config => config.IsEnabled).NotNull().WithMessage("attribute is missing or value is not provided")
-                     .DependentRules(() =>
-                     {
-                         RuleFor(config => config.IsEnabled).Must(x => x == false || x == true).WithMessage("attribute value is invalid. Expected value is either true or false");
-                     });
+            RuleFor(config => config.EncCellNames)
+               .Must(encs => encs != null && encs.Count() > 0 && encs.All(enc => !string.IsNullOrWhiteSpace(enc)))
+               .WithMessage("Attribute is missing or value is not provided");
 
-            RuleFor(b => b.Type).NotNull().WithMessage("attribute is missing")
-                //.DependentRules(() =>
-                //{
-                //    RuleFor(config => config.Type).
-                //NotEmpty().WithMessage("value is not provided")
-                .DependentRules(() =>
-                {
-                    RuleFor(config => config.Type).Must(type => IsValidBESType(type.ToUpper())).WithMessage("attribute value is invalid. Expected value is either BASE, CHANGE or UPDATE");
-                    //});
-                });
+            RuleFor(config => config.Frequency).NotNull().WithMessage("Attribute is missing or value is not provided")
+                    .DependentRules(() =>
+                    {
+                        RuleFor(config => config.Frequency).Must(f => IsValidCron(f)).WithMessage("Attribute value is invalid.");
+                    });
 
-            RuleFor(b => b.KeyFileType).NotNull().WithMessage("attribute is missing")
-               //.DependentRules(() =>
-               //{
-               //    RuleFor(config => config.KeyFileType).
-               //NotEmpty().WithMessage("value is not provided")
+            RuleFor(config => config.Type).NotNull().WithMessage("Attribute is missing")
                .DependentRules(() =>
                {
-                   RuleFor(config => config.KeyFileType).Must(keyFileType => IsValidKeyFileType(keyFileType.ToUpper())).WithMessage("attribute value is invalid. Expected value is KEY_TEXT, KEY_XML, PERMIT_XML or NONE");
-                   //});
+                   RuleFor(config => config.Type).Must(type => IsValidBESType(type.ToUpper())).WithMessage("Attribute value is invalid. Expected value is either BASE, CHANGE or UPDATE");
                });
 
-            RuleFor(b => b.BatchExpiryInDays).NotNull().WithMessage("attribute is missing")
+            RuleFor(config => config.KeyFileType).NotNull().WithMessage("Attribute is missing")
+               .DependentRules(() =>
+               {
+                   RuleFor(config => config.KeyFileType).Must(keyFileType => IsValidKeyFileType(keyFileType.ToUpper())).WithMessage("Attribute value is invalid. Expected value is KEY_TEXT, KEY_XML, PERMIT_XML or NONE");
+               });
+
+            RuleFor(c => c.AllowedUserGroups).Must((c, s) => IsAclProvided(c)).WithMessage("AllowedUsers and AllowedUserGroups both attributes values are not provided. Either of them should be provided").When(c => c.AllowedUserGroups != null && c.AllowedUsers != null);
+
+            RuleFor(c => c.AllowedUsers).Must((c, s) => IsAclProvided(c)).WithMessage("AllowedUsers and AllowedUserGroups both attributes values are not provided. Either of them should be provided")
+            .When(c => c.AllowedUserGroups != null && c.AllowedUsers != null);
+
+            RuleFor(config => config.Tags)
+                .Must(tags => tags != null && tags.Count() > 0).WithMessage("Attribute is missing or value not provided")
                 .DependentRules(() =>
                 {
-                    RuleFor(config => config.BatchExpiryInDays).NotEmpty().WithMessage("value is not provided")
+                    RuleFor(config => config.Tags)
+                    .Must(tags => tags.All(tag => !string.IsNullOrWhiteSpace(tag?.key) && !string.IsNullOrWhiteSpace(tag?.value)))
+                 .WithMessage("Key or Value not provided");
+                });
+
+            RuleFor(config => config.ReadMeSearchFilter)
+                .Must(readMeSearchFilter => !string.IsNullOrWhiteSpace(readMeSearchFilter))
+                .WithMessage("Attribute is missing or value not provided");
+
+            RuleFor(b => b.BatchExpiryInDays).NotNull().WithMessage("Attribute is missing")
+                .DependentRules(() =>
+                {
+                    RuleFor(config => config.BatchExpiryInDays).NotEmpty().WithMessage("Value is not provided")
                 .DependentRules(() =>
                 {
                     RuleFor(config => config.BatchExpiryInDays).GreaterThan(0).WithMessage("Expected value is natural number, i.e. number greater than 0");
                 });
                 });
 
-            RuleFor(b => b.ReadMeSearchFilter)
-                .Must(b => !string.IsNullOrWhiteSpace(b))
-                .WithMessage("attribute is missing or value not provided");
-
-            RuleFor(config => config.EncCellNames)
-               .Must(encs => encs != null && encs.Count() > 0 && encs.All(enc => !string.IsNullOrWhiteSpace(enc)))
-               .WithMessage("attribute is missing or value is not provided");
-
-            RuleFor(config => config.Tags)//.NotNull().NotEmpty().WithMessage("attribute is missing")
-                .Must(tags => tags != null && tags.Count() > 0).WithMessage("attribute is missing or value not provided")
-                .DependentRules(() =>
-                {
-                    RuleFor(config => config.Tags)
-                    .Must(tags => tags.All(tag => !string.IsNullOrWhiteSpace(tag?.key) && !string.IsNullOrWhiteSpace(tag?.value)))
-                 .WithMessage("key or value not provided");
-                });
-
-
-            RuleFor(config => config.EncCellNames)
-               .Must(encs => encs != null && encs.Count() > 0 && encs.All(enc => !string.IsNullOrWhiteSpace(enc)))
-               .WithMessage("attribute is missing or value is not provided");
-
-            //RuleFor(config => config.AllowedUsers.Length <1 && config.AllowedUserGroups.Length<1)
-            //  .Must(users => users.All()))
-            //.WithMessage("Allowed user is not in valid format.");
-
-            RuleFor(c => c.AllowedUserGroups).Must((c, s) => IsAclProvided(c)).WithMessage("AllowedUsers and AllowedUserGroups both attributes values are not provided. Either of them should be provided").When(c => c.AllowedUserGroups != null && c.AllowedUsers != null);
-
-            RuleFor(c => c.AllowedUsers).Must((c, s) => IsAclProvided(c)).WithMessage("AllowedUsers and AllowedUserGroups both attributes values are not provided. Either of them should be provided")
-            .When(c => c.AllowedUserGroups != null && c.AllowedUsers != null);
+            RuleFor(config => config.IsEnabled).NotNull().WithMessage("Attribute is missing or value is not provided")
+                     .DependentRules(() =>
+                     {
+                         RuleFor(config => config.IsEnabled).Must(isEnabled => isEnabled == false || isEnabled == true).WithMessage("Attribute value is invalid. Expected value is either true or false");
+                     });
         }
-        private bool IsAclProvided(BessConfig c)
+        private static bool IsAclProvided(BessConfig c)
         {
-            if (c.AllowedUsers.Count() == 0 && c.AllowedUserGroups.Count() == 0)
-                return false;
-            return true;
+            return (c.AllowedUsers.Count() == 0 && c.AllowedUserGroups.Count() == 0) ? false : true;
         }
-        bool IsValidName(string text)
+
+        static bool IsValidName(string text)
         {
             char[] SpecialChars = "\\/:*?\"<>|".ToCharArray();
 
             int indexOf = text.IndexOfAny(SpecialChars);
-            if (indexOf == -1)
-            {
-                // No special chars
-                return true;
-            }
-            return false;
+            return indexOf == -1;
         }
 
-        private bool IsValidBESType(string type)
+        private static bool IsValidBESType(string type)
         {
-            if (Enum.IsDefined(typeof(BESType), type))
-                return true;
-            return false;
+            return Enum.IsDefined(typeof(BESType), type);
         }
 
-        private bool IsValidKeyFileType(string keyFileType)
+        private static bool IsValidKeyFileType(string keyFileType)
         {
-            if (Enum.IsDefined(typeof(KeyFileType), keyFileType))
-                return true;
-            return false;
+            return Enum.IsDefined(typeof(KeyFileType), keyFileType);
         }
 
-        private bool IsValidExchangeSetStandard(string exchangeSetStandard)
+        private static bool IsValidExchangeSetStandard(string exchangeSetStandard)
         {
-            if (Enum.IsDefined(typeof(ExchangeSetStandard), exchangeSetStandard))
-                return true;
-            return false;
+            return Enum.IsDefined(typeof(ExchangeSetStandard), exchangeSetStandard);
+        }
+
+        private static bool IsValidCron(string frequency)
+        {
+            //return CrontabSchedule.TryParse(frequency, CronStringFormat.Default) != null;
+            return CrontabSchedule.TryParse(frequency, CronStringFormat.Default) == null ? false : true;
         }
         ValidationResult IConfigValidator.Validate(BessConfig bessConfig)
         {
