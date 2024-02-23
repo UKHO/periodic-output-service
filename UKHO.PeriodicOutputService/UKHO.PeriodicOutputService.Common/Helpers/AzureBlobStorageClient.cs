02ceeee714b0d34d6de452e8d14076a9713642ae
Abhijeet Naik <abhijee14992@mastek.com>
2024-02-23 13:44:46 +0530

    140033 - Changed log as per review comment AB#140033 AB#144878

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/AzureBlobStorageClient.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/AzureBlobStorageClient.cs
@@ -39,7 +39,7 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
             }
             catch (Exception ex)
             {
-                logger.LogError(EventIds.BessErrorOccurredWhileDownloadingConfigFromAzureStorage.ToEventId(), "Exception occurred while downloading configs from azure storage with Exception Message : {Message} | StackTrace : {StackTrace} | _X-Correlation-ID : {CorrelationId}", ex.Message, ex.StackTrace, CommonHelper.CorrelationID);
+                logger.LogError(EventIds.BessErrorOccurredWhileDownloadingConfigFromAzureStorage.ToEventId(), "Exception occurred while downloading configs of json file from azure storage container with Exception Message : {Message} | StackTrace : {StackTrace} | _X-Correlation-ID : {CorrelationId}", ex.Message, ex.StackTrace, CommonHelper.CorrelationID);
             }
             return configs;
         }
