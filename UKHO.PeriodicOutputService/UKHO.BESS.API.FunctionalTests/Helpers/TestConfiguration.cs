58bcb06027b07e8160596b09aa76324394adde81
Mayuresh Gawde <mayuresh10661@mastek.com>
2024-03-04 18:39:16 +0530

    142682 Fixed review comment AB#142682

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/TestConfiguration.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/TestConfiguration.cs
@@ -10,8 +10,8 @@ namespace UKHO.BESS.API.FunctionalTests.Helpers
         public class BessApiConfiguration
         {
             public string? BaseUrl { get; set; }
-            public string? ValidConfigPath { get; set; }
-            public string? InvalidConfigPath { get; set; }
+            public string ValidConfigPath { get; set; }
+            public string InvalidConfigPath { get; set; }
         }
 
         public TestConfiguration()
