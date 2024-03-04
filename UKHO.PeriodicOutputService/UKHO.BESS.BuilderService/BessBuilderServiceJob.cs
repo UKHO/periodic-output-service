14a187a3e9b8572ec6edfdbbfe6eafeb64745f67
Rupali Margale <rupali15127@mastek.com>
2024-03-04 17:40:13 +0530

    142689 - code refactored AB#142689 AB#143252

--- UKHO.PeriodicOutputService/UKHO.BESS.BuilderService/BessBuilderServiceJob.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.BuilderService/BessBuilderServiceJob.cs
@@ -6,20 +6,20 @@ namespace UKHO.BESS.BuilderService
 {
     public class BessBuilderServiceJob
     {
-        private readonly ILogger<BessBuilderServiceJob> _logger;
+        private readonly ILogger<BessBuilderServiceJob> logger;
         public BessBuilderServiceJob(ILogger<BessBuilderServiceJob> logger)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task CreateBespokeExchangeSetAsync()
         {
-            _logger.LogInformation(EventIds.BessBuilderServiceStarted.ToEventId(),
+            logger.LogInformation(EventIds.BessBuilderServiceStarted.ToEventId(),
                 "Bess Builder Service Started | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
 
             await Task.CompletedTask; // temporary code
 
-            _logger.LogInformation(EventIds.BessBuilderServiceCompleted.ToEventId(),
+            logger.LogInformation(EventIds.BessBuilderServiceCompleted.ToEventId(),
                 "Bess Builder Service Completed | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
         }
     }
