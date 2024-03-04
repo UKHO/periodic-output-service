bf1d033f7917ae6f9cc85c94098cb7e70fa0f32f
Mayuresh Gawde <mayuresh10661@mastek.com>
2024-03-04 12:54:45 +0530

    142682 Updated FT config AB#142682

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/FunctionalTests/MockEndPoint/UploadBessConfigFileTests.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/FunctionalTests/MockEndPoint/UploadBessConfigFileTests.cs
@@ -12,14 +12,14 @@ namespace UKHO.BESS.API.FunctionalTests.FunctionalTests.MockEndPoint
         [Test]
         public async Task WhenICallUploadConfigApiWithValidConfigFile_ThenCreatedStatusCode201IsReturned()
         {
-            HttpResponseMessage apiResponse = await BessUploadFileHelper.UploadConfigFile(testConfiguration.bessConfig.BaseUrl, @"./TestData/ConfigFile.json");
+            HttpResponseMessage apiResponse = await BessUploadFileHelper.UploadConfigFile(testConfiguration.bessConfig.BaseUrl, testConfiguration.bessConfig.ValidConfigPath);
             apiResponse.StatusCode.Should().Be((HttpStatusCode)201);
         }
 
         [Test]
         public async Task WhenICallUploadConfigApiWithInvalidConfigFile_ThenBadRequestStatusCode400IsReturned()
         {
-            HttpResponseMessage apiResponse = await BessUploadFileHelper.UploadConfigFile(testConfiguration.bessConfig.BaseUrl, @"./TestData/InvalidConfigFile.json");
+            HttpResponseMessage apiResponse = await BessUploadFileHelper.UploadConfigFile(testConfiguration.bessConfig.BaseUrl, testConfiguration.bessConfig.InvalidConfigPath);
             apiResponse.StatusCode.Should().Be((HttpStatusCode)400);
         }
     }
