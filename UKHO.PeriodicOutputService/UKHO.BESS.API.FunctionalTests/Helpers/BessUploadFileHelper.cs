3769ec010982a242fad1ba3235621e6b0d6c52f1
Arit Sarkar <arit14975@mastek.com>
2024-03-06 19:41:46 +0530

    Updated FT

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/BessUploadFileHelper.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/BessUploadFileHelper.cs
@@ -9,7 +9,7 @@ namespace UKHO.BESS.API.FunctionalTests.Helpers
         static readonly HttpClient httpClient = new();
         public static async Task<HttpResponseMessage> UploadConfigFile(string baseUrl, string path, string value)
         {
-            var uri = $"{baseUrl}/bessConfigUpload&Key={value}";
+            var uri = $"{baseUrl}/bessConfigUpload?Key={value}";
             var payloadJson = JsonConvert.SerializeObject(GetPayload(path));
             using (var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri)
             { Content = new StringContent(payloadJson, Encoding.UTF8, "application/json") })
