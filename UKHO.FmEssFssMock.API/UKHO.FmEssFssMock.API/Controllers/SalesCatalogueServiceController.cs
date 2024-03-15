9a9dd57e6ceeec75ed368f8a631301eeffd7f6a2
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-03-15 16:41:15 +0530

    148824-add scs mock controller and required services AB#148824

---
+++ UKHO.FmEssFssMock.API/UKHO.FmEssFssMock.API/Controllers/SalesCatalogueServiceController.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using UKHO.FmEssFssMock.API.Models.Request;
+using UKHO.FmEssFssMock.API.Services;
+
+namespace UKHO.FmEssFssMock.API.Controllers
+{
+    [ApiController]
+    public class SalesCatalogueServiceController : BaseController
+    {
+        private readonly SalesCatalogueService salesCatalogueService;
+
+        public Dictionary<string, string> ErrorsIdentifiers { get; set; }
+        public Dictionary<string, string> ErrorsVersions { get; set; }
+        public Dictionary<string, string> ErrorsSinceDateTime { get; set; }
+
+        public SalesCatalogueServiceController(IHttpContextAccessor httpContextAccessor, SalesCatalogueService salesCatalogueService) : base(httpContextAccessor)
+        {
+            this.salesCatalogueService = salesCatalogueService;
+            ErrorsIdentifiers = new Dictionary<string, string>
+            {
+                { "source", "productIds" },
+                { "description", "None of the product Ids exist in the database" }
+            };
+            ErrorsVersions = new Dictionary<string, string>
+            {
+                { "source", "productVersions" },
+                { "description", "None of the product Ids exist in the database" }
+            };
+            ErrorsSinceDateTime = new Dictionary<string, string>
+            {
+                { "source", "productSinceDateTime" },
+                { "description", "None of the product Ids exist in the database" }
+            };
+        }
+
+        [HttpGet]
+        [Route("v1/productData/encs57/products")]
+        public IActionResult ProductsSinceDateTime(string sinceDateTime)
+        {
+            if (!string.IsNullOrEmpty(sinceDateTime))
+            {
+                var response = salesCatalogueService.GetProductSinceDateTime(sinceDateTime);
+                if (response != null)
+                {
+                    return Ok(response.ResponseBody);
+                }
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [Route("v1/productData/encs57/products/productIdentifiers")]
+        public IActionResult ProductIdentifiers(List<string> productIdentifiers)
+        {
+            if (productIdentifiers != null && productIdentifiers.Any())
+            {
+                var response = salesCatalogueService.GetProductIdentifier("productIdentifier-" + String.Join("-", productIdentifiers));
+                if (response != null)
+                {
+                    return Ok(response.ResponseBody);
+                }
+            }
+            return BadRequest(new { CorrelationId = GetCurrentCorrelationId(), Errors = ErrorsIdentifiers });
+        }
+
+        [HttpPost]
+        [Route("v1/productData/encs57/products/productVersions")]
+        public IActionResult ProductVersions(List<ProductVersionRequest> productVersionRequest)
+        {
+            if (productVersionRequest != null && productVersionRequest.Any())
+            {
+                var productVersionRequestSearchText = new StringBuilder();
+                bool isInitalIndex = true;
+                const string NotModifiedProductName = "DE416040";
+                const int NotModifiedEditionNumber = 11;
+                const int NotModifiedUpdateNumber = 1;
+                foreach (var item in productVersionRequest)
+                {
+                    //code added to handle 304 not modified scenario
+                    if (item.ProductName == NotModifiedProductName && item.EditionNumber == NotModifiedEditionNumber && item.UpdateNumber == NotModifiedUpdateNumber)
+                    {
+                        return StatusCode(StatusCodes.Status304NotModified);
+                    }
+                    productVersionRequestSearchText.Append((isInitalIndex ? "" : "-") + item.ProductName + "-" + item.EditionNumber + "-" + item.UpdateNumber);
+                    isInitalIndex = false;
+                }
+                var response = salesCatalogueService.GetProductVersion("productVersion-" + productVersionRequestSearchText.ToString());
+                if (response != null)
+                {
+                    return Ok(response.ResponseBody);
+                }
+            }
+            return BadRequest(new { CorrelationId = GetCurrentCorrelationId(), Errors = ErrorsVersions });
+        }
+
+        [HttpGet]
+        [Route("v1/productData/encs57/catalogue/essData")]
+        public IActionResult GetEssData()
+        {
+            var response = salesCatalogueService.GetEssData();
+            if (response != null)
+            {
+                return Ok(response.ResponseBody);
+            }
+            return BadRequest(new { CorrelationId = GetCurrentCorrelationId(), Errors = ErrorsVersions });
+        }
+    }
+}
---
+++ UKHO.FmEssFssMock.API/UKHO.FmEssFssMock.API/Models/Response/Products.cs
@@ -0,0 +1,19 @@
+﻿namespace UKHO.FmEssFssMock.API.Models.Response
+{
+    public class Products
+    {
+        public string ProductName { get; set; }
+        public int? EditionNumber { get; set; }
+        public List<int?> UpdateNumbers { get; set; }
+        public List<Dates> Dates { get; set; }
+        public Cancellation Cancellation { get; set; }
+        public int? FileSize { get; set; }
+        public List<Bundle> Bundle { get; set; }
+    }
+
+    public class Bundle
+    {
+        public string BundleType { get; set; }
+        public string Location { get; set; }
+    }
+}
