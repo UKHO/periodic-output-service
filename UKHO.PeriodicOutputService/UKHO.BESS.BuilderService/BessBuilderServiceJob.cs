80d01f2294d1a982962aaa5c173ad9025a75e2fd
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-03-25 21:37:19 +0530

    147129-new branch based on dev/147129-bess-bs-read-queue-message AB#147129

--- UKHO.PeriodicOutputService/UKHO.BESS.BuilderService/BessBuilderServiceJob.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.BuilderService/BessBuilderServiceJob.cs
@@ -1,27 +1,49 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.CodeAnalysis;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using UKHO.PeriodicOutputService.Common.Helpers;
 using UKHO.PeriodicOutputService.Common.Logging;
+using UKHO.PeriodicOutputService.Common.Models.Bess;
 
 namespace UKHO.BESS.BuilderService
 {
+    [ExcludeFromCodeCoverage]
     public class BessBuilderServiceJob
     {
         private readonly ILogger<BessBuilderServiceJob> logger;
+        private IConfiguration configuration;
 
-        public BessBuilderServiceJob(ILogger<BessBuilderServiceJob> logger)
+        public BessBuilderServiceJob(ILogger<BessBuilderServiceJob> logger, IConfiguration configuration)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public async Task CreateBespokeExchangeSetAsync()
+        public async Task ProcessQueueMessage([QueueTrigger("%BessStorageConfiguration:QueueName%")] QueueMessage message)
         {
-            logger.LogInformation(EventIds.BessBuilderServiceStarted.ToEventId(),
-                "Bess Builder Service Started | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
+            try
+            {
+                string keyvaultUrl = configuration.GetValue<string>("KeyVaultSettings:ServiceUri");
 
-            await Task.CompletedTask; // temporary code
+                string queueName = configuration.GetValue<string>("BessStorageConfiguration:QueueName");
 
-            logger.LogInformation(EventIds.BessBuilderServiceCompleted.ToEventId(),
-                "Bess Builder Service Completed | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
+                logger.LogInformation(EventIds.BessBuilderServiceStarted.ToEventId(),
+                    "Bess Builder Service Started, Queuename:{Queuename}, KeyVaultUrl:{KeyVaultUrl} | _X-Correlation-ID : {CorrelationId}", queueName, keyvaultUrl, CommonHelper.CorrelationID);
+
+                ConfigQueueMessage configQueueMessage = message.Body.ToObjectFromJson<ConfigQueueMessage>();
+
+                await Task.CompletedTask; // temporary code
+
+                logger.LogInformation(EventIds.BessBuilderServiceCompleted.ToEventId(),
+                    "Bess Builder Service Completed | _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(EventIds.UnhandledException.ToEventId(), "Exception occurred while processing Bess Builder Service webjob with Exception Message : {Message} | StackTrace : {StackTrace} | _X-Correlation-ID : {CorrelationId}", ex.Message, ex.StackTrace, CommonHelper.CorrelationID);
+                throw;
+            }
         }
     }
 }
