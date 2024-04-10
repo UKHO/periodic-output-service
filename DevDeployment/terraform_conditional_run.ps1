e5295727f9fcce4ae6c483a682e5b408bb905c17
Surendra Behara <surendr11609@mastek.com>
2024-04-10 20:04:13 +0530

    150897 - updated variable name AB#150897

--- DevDeployment/terraform_conditional_run.ps1
+++ DevDeployment/terraform_conditional_run.ps1
@@ -71,6 +71,7 @@ Write-Host "##vso[task.setvariable variable=BessStorageConfiguration.ConnectionS
 Write-Host "##vso[task.setvariable variable=EventHubLoggingConfiguration.ConnectionString;issecret=true]$($terraformOutput.log_primary_connection_string.value)"
 Write-Host "##vso[task.setvariable variable=EventHubLoggingConfiguration.EntityPath;issecret=true]$($terraformOutput.entity_path.value)"
 Write-Host "##vso[task.setvariable variable=ApplicationInsights.ConnectionString;issecret=true]$($terraformOutput.connection_string.value)"
-Write-Host "##vso[task.setvariable variable=AzureWebJobsStorage;issecret=true;isOutput=true]$($terraformOutput.bess_storage_connection_string.value)"
+Write-Host "##vso[task.setvariable variable=AzureWebJobsStorage;issecret=true]$($terraformOutput.bess_storage_connection_string.value)"
+Write-Host "##vso[task.setvariable variable=AzureWebJobsStorageName;issecret=true;isOutput=true]$($terraformOutput.bess_storage_connection_string.value)"
 
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
