18702ac9bf12fce76ba239848bf408f01087f442
Arit Sarkar <arit14975@mastek.com>
2024-04-15 13:19:52 +0530

    Updated .yml file

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/EssEndpointHelper.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/EssEndpointHelper.cs
@@ -57,7 +57,7 @@ namespace UKHO.BESS.API.FunctionalTests.Helpers
         public static async Task<string> CreateExchangeSetFile(string batchId)
         {
             // The below sleep is to give time to BuilderService to download the required zip
-            Thread.Sleep(90000);
+            Thread.Sleep(120000);
             string finalBatchStatusUrl = $"{configs.fssConfig.BaseUrl}/batch/{batchId}/status";
             Console.WriteLine(finalBatchStatusUrl);
             string batchStatus = await FssBatchHelper.CheckBatchIsCommitted(finalBatchStatusUrl);
