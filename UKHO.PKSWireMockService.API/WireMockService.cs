f4b355335b74673bf8bcee5694503871568b9048
Shirin Talawdekar <shirin14926@mastek.com>
2024-04-17 13:02:58 +0530

    152477 fix few vulnerability and code upliftment AB#152477

--- UKHO.PKSWireMockService.API/WireMockService.cs
+++ UKHO.PKSWireMockService.API/WireMockService.cs
@@ -25,36 +25,21 @@ namespace UKHO.PKSWireMock.API
                 _logger = logger;
             }
 
-            public void Debug(string formatString, params object[] args)
-            {
-                _logger.LogDebug(formatString, args);
-            }
+            public void Debug(string formatString, params object[] args) => _logger.LogDebug(formatString, args);
 
-            public void Info(string formatString, params object[] args)
-            {
-                _logger.LogInformation(formatString, args);
-            }
+            public void Info(string formatString, params object[] args) => _logger.LogInformation(formatString, args);
 
-            public void Warn(string formatString, params object[] args)
-            {
-                _logger.LogWarning(formatString, args);
-            }
+            public void Warn(string formatString, params object[] args) => _logger.LogWarning(formatString, args);
 
-            public void Error(string formatString, params object[] args)
-            {
-                _logger.LogError(formatString, args);
-            }
+            public void Error(string formatString, params object[] args) => _logger.LogError(formatString, args);
 
             public void DebugRequestResponse(LogEntryModel logEntryModel, bool isAdminrequest)
             {
-                string message = JsonConvert.SerializeObject(logEntryModel, Newtonsoft.Json.Formatting.Indented);
+                string message = JsonConvert.SerializeObject(logEntryModel, Formatting.Indented);
                 _logger.LogDebug("Admin[{0}] {1}", isAdminrequest, message);
             }
 
-            public void Error(string formatString, Exception exception)
-            {
-                _logger.LogError(formatString, exception.Message);
-            }
+            public void Error(string formatString, Exception exception) => _logger.LogError(formatString, exception.Message);
         }
 
         public WireMockService(ILogger<WireMockService> logger, IOptions<WireMockServerSettings> settings)
