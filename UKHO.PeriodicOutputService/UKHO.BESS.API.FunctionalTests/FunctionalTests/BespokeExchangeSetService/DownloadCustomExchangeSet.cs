01e3384b8498e359f3f2c3e054a40c0286cacddc
Surendra Behara <surendr11609@mastek.com>
2024-04-25 13:39:05 +0530

    140039 - added swap task for Ft's AB#140039 AB#154946

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/FunctionalTests/BespokeExchangeSetService/DownloadCustomExchangeSet.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/FunctionalTests/BespokeExchangeSetService/DownloadCustomExchangeSet.cs
@@ -10,7 +10,6 @@ using static UKHO.BESS.API.FunctionalTests.Helpers.TestConfiguration;
 
 namespace UKHO.BESS.API.FunctionalTests.FunctionalTests.BespokeExchangeSetService
 {
-    [Ignore("Temp")]
     [TestFixture]
     public class RequestAndDownloadExchangeSet
     {
