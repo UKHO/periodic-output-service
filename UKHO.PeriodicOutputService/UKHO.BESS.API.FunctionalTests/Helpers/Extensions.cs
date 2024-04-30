9c26360d5d0ac88195004741a2285e931bdce65e
Mayuresh Gawde <mayuresh10661@mastek.com>
2024-04-30 13:24:28 +0530

    140039 Fixing failing FT AB#140039

--- UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/Extensions.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.API.FunctionalTests/Helpers/Extensions.cs
@@ -38,7 +38,7 @@
         public static void WaitForDownloadExchangeSet()
         {
             //The below sleep is to give time to BuilderService to download the exchangeSet.
-            Thread.Sleep(180000);
+            Thread.Sleep(210000);
         }
 
         /// <summary>
