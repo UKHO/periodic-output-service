c63a2a23c3258aae4dd50d21fdeb99e093774b78
Ratnashree Chavan <ratnas104142@mastek.com>
2024-05-06 19:32:44 +0530

    140043 - Modification AB#140043

--- UKHO.PeriodicOutputService/UKHO.BESS.CleanUpJob/BessCleanUpJob.cs
+++ UKHO.PeriodicOutputService/UKHO.BESS.CleanUpJob/BessCleanUpJob.cs
@@ -10,19 +10,19 @@ namespace UKHO.BESS.CleanUpJob
     public class BessCleanUpJob
     {
         private readonly ILogger<BessCleanUpJob> logger;
-        private readonly ICleanUpService bespokeExchangeSetCleanUpService;
+        private readonly ICleanUpService bessCleanUpService;
 
-        public BessCleanUpJob(ILogger<BessCleanUpJob> logger, ICleanUpService bespokeExchangeSetCleanUpService)
+        public BessCleanUpJob(ILogger<BessCleanUpJob> logger, ICleanUpService bessCleanUpService)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.bespokeExchangeSetCleanUpService = bespokeExchangeSetCleanUpService ?? throw new ArgumentNullException(nameof(bespokeExchangeSetCleanUpService));
+            this.bessCleanUpService = bessCleanUpService ?? throw new ArgumentNullException(nameof(bessCleanUpService));
         }
 
         public async Task ProcessCleanUp()
         {
             logger.LogInformation(EventIds.BESSCleanUpJobRequestStarted.ToEventId(), "Bespoke Exchange set service clean up web job started at " + DateTime.Now + "| _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
 
-            await bespokeExchangeSetCleanUpService.CleanUpHistoricFoldersAndFiles();
+            await bessCleanUpService.CleanUpHistoricFoldersAndFiles();
 
             logger.LogInformation(EventIds.BESSCleanUpJobRequestCompleted.ToEventId(), "Bespoke Exchange set service clean up web job completed at " + DateTime.Now + "| _X-Correlation-ID : {CorrelationId}", CommonHelper.CorrelationID);
         }
