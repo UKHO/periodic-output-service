c78f88097d2bad47fcde4990f2d09596c4f61b4c
Ratnashree Chavan <ratnas104142@mastek.com>
2024-05-15 19:51:33 +0530

    154171 - Stryker improvement AB#154171

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/SalesCatalogueClient.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/SalesCatalogueClient.cs
@@ -5,10 +5,11 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
     public class SalesCatalogueClient : ISalesCatalogueClient
     {
         private HttpClient httpClient;
+        private const string SCSCLIENT = "ScsClient";
 
         public SalesCatalogueClient(IHttpClientFactory httpClientFactory)
         {
-            httpClient = httpClientFactory.CreateClient("ScsClient");
+            httpClient = httpClientFactory.CreateClient(SCSCLIENT);
             httpClient.MaxResponseContentBufferSize = 2147483647;
             httpClient.Timeout = TimeSpan.FromMinutes(Convert.ToDouble(5));
         }
@@ -20,8 +21,8 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
             if (requestBody != null)
                 content = new StringContent(requestBody, Encoding.UTF8, "application/json");
 
-            using var httpRequestMessage = new HttpRequestMessage(method, uri)
-            { Content = content };
+            using var httpRequestMessage = new HttpRequestMessage(method, uri);
+            httpRequestMessage.Content = content;
 
             httpRequestMessage.AddCorrelationId(CommonHelper.CorrelationID.ToString());
             httpRequestMessage.SetBearerToken(authToken);
