8e07534f3706675e4862ff8f830b6718224b4041
Chaitali Shidid <chaital14503@mastek.com>
2024-05-21 19:46:59 +0530

    correlation id changes

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/EssApiClient.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Helpers/EssApiClient.cs
@@ -15,7 +15,7 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
             _httpClient.Timeout = TimeSpan.FromMinutes(Convert.ToDouble(5));
         }
 
-        public async Task<HttpResponseMessage> PostProductIdentifiersDataAsync(string uri, List<string> productIdentifierModel, string accessToken)
+        public async Task<HttpResponseMessage> PostProductIdentifiersDataAsync(string uri, List<string> productIdentifierModel, string accessToken, string? correlationId = null)
         {
             string payloadJson = JsonConvert.SerializeObject(productIdentifierModel);
 
@@ -27,13 +27,13 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
                 if (accessToken != null)
                 {
                     httpRequestMessage.SetBearerToken(accessToken);
-                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.CorrelationID.ToString());
+                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.GetCorrelationId(correlationId));
                 }
                 return await _httpClient.SendAsync(httpRequestMessage, CancellationToken.None);
             }
         }
 
-        public async Task<HttpResponseMessage> GetProductDataSinceDateTime(string uri, string sinceDateTime, string accessToken)
+        public async Task<HttpResponseMessage> GetProductDataSinceDateTime(string uri, string sinceDateTime, string accessToken, string? correlationId = null)
         {
             string payloadJson = JsonConvert.SerializeObject(sinceDateTime);
 
@@ -45,14 +45,14 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
                 if (accessToken != null)
                 {
                     httpRequestMessage.SetBearerToken(accessToken);
-                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.CorrelationID.ToString());
+                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.GetCorrelationId(correlationId));
                 }
 
                 return await _httpClient.SendAsync(httpRequestMessage, CancellationToken.None);
             }
         }
 
-        public async Task<HttpResponseMessage> GetProductDataProductVersion(string uri, List<ProductVersion> productVersions, string accessToken)
+        public async Task<HttpResponseMessage> GetProductDataProductVersion(string uri, List<ProductVersion> productVersions, string accessToken, string? correlationId = null)
         {
             string payloadJson = JsonConvert.SerializeObject(productVersions);
 
@@ -64,7 +64,7 @@ namespace UKHO.PeriodicOutputService.Common.Helpers
                 if (accessToken != null)
                 {
                     httpRequestMessage.SetBearerToken(accessToken);
-                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.CorrelationID.ToString());
+                    httpRequestMessage.AddHeader("X-Correlation-ID", CommonHelper.GetCorrelationId(correlationId));
                 }
 
                 return await _httpClient.SendAsync(httpRequestMessage, CancellationToken.None);
