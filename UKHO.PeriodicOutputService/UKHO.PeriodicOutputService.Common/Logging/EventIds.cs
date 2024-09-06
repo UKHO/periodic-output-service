4acf4e8a8ab7ef08d2a268c96cdc741db5ce47fc
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-09-06 08:09:03 +0100

    Feature/updates to pos for bess (#208)
    * Change method to create blob if blob does not exist
    
    * Remove functional tests from QA
    
    * Add some logging for queue and blob creation
    
    * Add correlation Id to logging message

--- UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Logging/EventIds.cs
+++ UKHO.PeriodicOutputService/UKHO.PeriodicOutputService.Common/Logging/EventIds.cs
@@ -833,7 +833,17 @@ namespace UKHO.PeriodicOutputService.Common.Logging
         /// <summary>
         ///     Bess Queue Created
         /// </summary>
-        BessQueueCreated = 809165
+        BessQueueCreated = 809165,
+
+        /// <summary>
+        ///     Blob Container Created
+        /// </summary>
+        BlobContainerCreated = 809166,
+
+        /// <summary>
+        ///     Error Creating container
+        /// </summary>
+        ContainerCreationFailure = 809167
     }
 
     public static class EventIdExtensions
