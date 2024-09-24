390adf253cc15628626683ef7115d7486141f939
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-09-24 10:19:14 +0000

    Update Terraform provider and Azure Pipelines container version

--- Deployment/src/azure.tf
+++ Deployment/src/azure.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.2.0"
+      version = "=3.116.0"
     }
   }
 
-  required_version = "=1.1.9"
+  required_version = "=1.9.6"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "posterraform.deployment.tfplan"
