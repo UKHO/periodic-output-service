17d6269f0245ce5be60fd407238c2277f3bbc4be
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-09-25 13:34:01 +0000

    Update Azure App Service Plan names for quality control environment

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -28,8 +28,8 @@ module "eventhub" {
 }
 
 data "azurerm_app_service_plan" "essft_asp" {
-  name                = "ess-${local.env_name}-sxs-2-asp"
-  resource_group_name = "ess-${local.env_name}-rg"
+  name                = "essft-qc-yh3r1-asp"
+  resource_group_name = "essft-qc-webapp-rg"
 }
 
 data "azurerm_app_service_plan" "ess_asp" {
