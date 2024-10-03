adcb8d8a56fcbb8560dda98a513bc3f8186f5f26
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-10-03 10:59:59 +0000

    Remove change

--- UKHO.FmEssFssMock.API/UKHO.FmEssFssMock.API/Services/ExchangeSetService.cs
+++ UKHO.FmEssFssMock.API/UKHO.FmEssFssMock.API/Services/ExchangeSetService.cs
@@ -25,7 +25,7 @@ namespace UKHO.FmEssFssMock.API.Services
             _fssService = fssService;
             _mockService = mockService;
 
-            _homeDirectoryPath = Path.Combine(configuration["HOME"], configuration["POSFolderName"], "ESS");
+            _homeDirectoryPath = Path.Combine(configuration["HOME"], configuration["POSFolderName"]);
         }
 
         public ExchangeSetServiceResponse CreateExchangeSetForGetProductDataSinceDateTime(string sinceDateTime, string exchangeSetStandard)
