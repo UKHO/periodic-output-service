f223082fa70f16b8455dd89ce4479154cdeef9ad
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-12-24 08:26:07 +0000

    Update outputs for webapps

--- Deployment/src/Modules/WebApp/outputs.tf
+++ Deployment/src/Modules/WebApp/outputs.tf
@@ -1,20 +1,20 @@
 output "web_app_object_id" {
-  value = azurerm_app_service.webapp_service.identity.0.principal_id
+  value = azurerm_windows_web_app.webapp_service.identity.0.principal_id
 }
 
 output "web_app_tenant_id" {
-  value = azurerm_app_service.webapp_service.identity.0.tenant_id
+  value = azurerm_windows_web_app.webapp_service.identity.0.tenant_id
 }
 
 output "default_site_hostname" {
-  value = azurerm_app_service.webapp_service.default_site_hostname
+  value = azurerm_windows_web_app.webapp_service.default_hostname
 }
 
 output "username" {
-  value = azurerm_app_service.webapp_service.site_credential[0].username
+  value = azurerm_windows_web_app.webapp_service.site_credential[0].name
 }
 
 output "password" {
-  value = azurerm_app_service.webapp_service.site_credential[0].password
+  value = azurerm_windows_web_app.webapp_service.site_credential[0].password
   sensitive = true
 }
