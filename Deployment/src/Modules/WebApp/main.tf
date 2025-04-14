9921929b606722cc58fae90c35fc1c59f8693def
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-04-14 12:00:10 +0100

    Revert swift connection change

--- Deployment/src/Modules/WebApp/main.tf
+++ Deployment/src/Modules/WebApp/main.tf
@@ -39,8 +39,7 @@ resource "azurerm_windows_web_app" "webapp_service" {
     ignore_changes = [ virtual_network_subnet_id ]
   }
 
-  https_only                = true
-  virtual_network_subnet_id = var.subnet_id
+  https_only = true
 }
 
 resource "azurerm_windows_web_app_slot" "staging" {
@@ -78,6 +77,16 @@ resource "azurerm_windows_web_app_slot" "staging" {
     ignore_changes = [ virtual_network_subnet_id ]
   }
 
-  https_only                = azurerm_windows_web_app.webapp_service.https_only
-  virtual_network_subnet_id = var.subnet_id
+  https_only = azurerm_windows_web_app.webapp_service.https_only
+}
+
+resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
+  app_service_id = azurerm_windows_web_app.webapp_service.id
+  subnet_id      = var.subnet_id
+}
+
+resource "azurerm_app_service_slot_virtual_network_swift_connection" "slot_vnet_integration" {
+  app_service_id = azurerm_windows_web_app.webapp_service.id
+  subnet_id      = var.subnet_id
+  slot_name      = azurerm_windows_web_app_slot.staging.name
 }
