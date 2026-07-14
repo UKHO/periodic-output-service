8fe4119371317647a476f5ca81524a374becafe5
Vishal14583 <vishal.dukare@mastek.com>
2026-07-14 14:23:22 +0530

    290760-devops changes for .net upgrade AB#290760

--- Deployment/src/azure.tf
+++ Deployment/src/azure.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.116.0"
+      version = ">=4.54.0"
     }
   }
 
@@ -15,10 +15,12 @@ terraform {
 
 provider "azurerm" {
   features {}
+  version = ">=4.54.0"
 }
 
 provider "azurerm" {
   features {}
   alias = "build_agent"
   subscription_id = var.agent_subscription_id
+  version = ">=4.54.0"
 }
