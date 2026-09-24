d824722aa39d5e757b3d2d4d1d37400736a608f5
MattWin90 <matthew.winstanley@mastek.com>
2026-09-24 16:45:13 +0100

    fixed reference to rg module name

--- Deployment/src/migrate.tf
+++ Deployment/src/migrate.tf
@@ -18,7 +18,7 @@ removed {
 
 import {
   to = module.storagePOS.azurerm_storage_account.pos_storage
-  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name}${var.env_name}storageukho")}"
+  id = "${azurerm_resource_group.webapp_rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name}${var.env_name}storageukho")}"
 }
 
 removed {
@@ -31,5 +31,5 @@ removed {
 
 import {
   to = module.storageBESS.azurerm_storage_account.bess_storage
-  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name_bess}${var.env_name}storageukho")}"
+  id = "${azurerm_resource_group.webapp_rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name_bess}${var.env_name}storageukho")}"
 }
