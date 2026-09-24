d74e42b290b8eb2671273ffdaa8bd79afec5dbda
MattWin90 <matthew.winstanley@mastek.com>
2026-09-24 15:53:56 +0100

    added sa migration

--- Deployment/src/migrate.tf
+++ Deployment/src/migrate.tf
@@ -6,3 +6,30 @@ removed {
     destroy = false
   }
 }
+
+#storage POS and BESS
+removed {
+  from = module.storage.azurerm_storage_account.pos_storage
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.storagePOS.azurerm_storage_account.pos_storage
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name}${var.env_name}storageukho")}"
+}
+
+removed {
+  from = module.storage.azurerm_storage_account.bess_storage
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.module.storageBESS.azurerm_storage_account.bess_storage
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Storage/storageAccounts/${lower("${var.service_name_bess}${var.env_name}storageukho")}"
+}
